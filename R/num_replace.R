c11afebc8f0960386c1d047f8d710aa6e1e09546
Tyler Rinker <tyler.rinker@gmail.com>
2012-06-03 17:18:54 -0400

    added roxygen template

--- R/num_replace.R
+++ R/num_replace.R
@@ -1,3 +1,19 @@
+#' Order a data frame by its columns.
+#'
+#' This function completes the subsetting, transforming and ordering triad
+#' with a function that works in a similar way to \code{\link{subset}} and 
+#' \code{\link{transform}} but for reordering a data frame by its columns.
+#' This saves a lot of typing!
+#'
+#' @param df data frame to reorder
+#' @param ... expressions evaluated in the context of \code{df} and 
+#'   then fed to \code{\link{order}}
+#' @keywords manip
+#' @export
+#' @examples
+#' mtcars[with(mtcars, order(cyl, disp)), ]
+#' arrange(mtcars, cyl, disp)
+#' arrange(mtcars, cyl, desc(disp))
 num_replace <-
 function(text.var, num.paste = "separate") {
     numb2word <- function(x, billion = c("US", "UK"), and = if (billion == 
