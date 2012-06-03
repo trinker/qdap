05773936fc8be0ddd77f36fed9ca041249a35f1e
Tyler Rinker <tyler.rinker@gmail.com>
2012-06-03 16:00:48 -0400

    Added roxygen templates to function headers for functions affix though gantt.plot

--- R/col.fn.R
+++ R/col.fn.R
@@ -1,3 +1,19 @@
+#' Order a data frame by its columns.
+#'
+#' This function completes the subsetting, transforming and ordering triad
+#' with a function that works in a similar way to \code{\link{subset}} and 
+#' \code{\link{transform}} but for reordering a data frame by its columns.
+#' This saves a lot of typing!
+#'
+#' @param df data frame to reorder
+#' @param ... expressions evaluated in the context of \code{df} and 
+#'   then fed to \code{\link{order}}
+#' @keywords manip
+#' @export
+#' @examples
+#' mtcars[with(mtcars, order(cyl, disp)), ]
+#' arrange(mtcars, cyl, disp)
+#' arrange(mtcars, cyl, desc(disp))
 col.fn <-
 function(col, alpha=0.3) {
     col<- hcl(col * 360, 130, 60)
