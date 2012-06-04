956b273ae0331acc50aa53d7dc071dc26fc70b33
Tyler Rinker <tyler.rinker@gmail.com>
2012-06-04 19:12:07 -0400

    added comment message (output and digits) to termco.d for use in termco.c.
    termco.c now takes both vector and list of vectors.

--- R/termco.c.R
+++ R/termco.c.R
@@ -19,79 +19,77 @@
 #' @references %% ~put references to the literature/web site here ~
 #' @keywords ~kwd1 ~kwd2
 #' @examples
-#' 
-#' ##---- Should be DIRECTLY executable !! ----
-#' ##-- ==>  Define data, use random,
-#' ##--	or do  help(data=index)  for the standard data sets.
-#' 
-#' ## The function is currently defined as
-#' function (termco.d.object, combined.columns, new.name, zero.replace = 0, 
-#'     elim.old = TRUE) 
-#' {
-#'     x <- termco.d.object$raw
-#'     y <- termco.d.object$prop
-#'     if (!is.numeric(combined.columns)) {
-#'         combined.columns <- which(names(x) %in%
-#'         combined.columns)
-#'     }
-#'     x <- transform(x, new.name = rowSums(x[, combined.columns]), 
-#'         check.names = FALSE)
-#'     names(x)[length(x)] <- names(y)[length(y)] <- new.name
-#'     y <- transform(y, new.name = rowSums(y[, combined.columns]), 
-#'         check.names = FALSE)
-#'     z <- if (elim.old) {
-#'         seq_along(x)[!seq_along(x) %in% combined.columns]
-#'     }
-#'     else {
-#'         seq_along(x)
-#'     }
-#'     x2 <- replacer(x, with = zero.replace)[, z]
-#'     y2 <- replacer(y, with = zero.replace)[, z]
-#'     DF <- replacer(termco.rnp(x, y), "0(0)", with = 
-#'         zero.replace)[, z]
-#'     o <- list(raw = x2, prop = y2, rnp = DF)
-#'     class(o) <- "termco_c"
-#'     return(o)
-#'   }
-#' 
+
 termco.c <-
 function(termco.d.object, combined.columns, new.name, 
          zero.replace = 0, elim.old = TRUE){ 
-    # browser ()
-    #find out if cc and nn are a list and vector or vector and single
-    #make fun here 
+    if (!class(termco.d.object) %in% c("termco.d.object", "termco.d.object")){
+        stop("termco.d.object must be a termco.d.object or termco.c.object")
+    }
     x <- termco.d.object$raw
+    xcheck <- names(x)
     y <- termco.d.object$prop
-    if (!is.numeric(combined.columns)){
-        combined.columns <- which(names(x) %in% combined.columns)
-    }
-    x <- transform(x, new.name = rowSums(x[, combined.columns]), 
-                   check.names=FALSE)
-    y <- transform(y, new.name = rowSums(y[, combined.columns]), 
-                   check.names=FALSE)
-    z <- if (elim.old) {
-        seq_along(x)[!seq_along(x) %in% combined.columns] 
+    if (!is.numeric(unlist(combined.columns))){
+        cc <- function(X, Y){
+            which(names(Y) %in% X)
+        }
+        if (!is.list(combined.columns)){
+            combined.columns2 <- cc(combined.columns, x)
+        } else {
+            combined.columns2 <- lapply(combined.columns, function(x2) 
+                cc(X=x2, Y=x)) 
+        }
     } else {
-        seq_along(x)
+        combined.columns2 <- combined.columns
+        cc <- function(X, Y) names(Y)[X]
+        if (!is.list(combined.columns)){
+            combined.columns <- cc(combined.columns, x)            
+        } else {
+            combined.columns <- lapply(combined.columns, function(x2) 
+                cc(X=x2, x)) 
+        }
     }
-    names(x)[length(x)] <- names(y)[length(y)] <- new.name
-    x2 <- replacer(x, with = zero.replace)[, z]
-    y2 <- replacer(y, with = zero.replace)[, z]
-    if (new.name %in% names(x)[combined.columns] & elim.old){
-        names(x2)[ncol(x2)] <- new.name
-        names(y2)[ncol(y2)] <- new.name
+    if (is.list(combined.columns)){
+        trx <- function(i) {  
+            x <- transform(x, new.name2 = rowSums(x[, combined.columns[[i]]]), 
+                           check.names=FALSE)
+            names(x)[ncol(x)] <- new.name[i]
+            return(x)
+        }
+        invisible(lapply(seq_along(combined.columns), function(inp) {
+            x <<- trx(inp)
+        }
+        )) 
+    } else {
+        x <- transform(x, new.name = rowSums(x[, combined.columns]), 
+                       check.names=FALSE)
+        names(x) [length(x)] <- new.name
+    } 
+    if (elim.old) {
+        x <- x[, seq_along(x)[!seq_along(x) %in% unlist(combined.columns2)]]
+    } 
+    x2 <- replacer(x, with = zero.replace)
+    cm <- comment(termco.d.object)
+    y2 <- termco.p(x, output = cm[1], digits=as.numeric(cm[2]))
+    if (any(new.name %in% xcheck) & elim.old){
+        #   names(x)[(ncol(x) - length(new.name) + 1):ncol(x)] <- new.name
+        names(x2)[(ncol(x2) - length(new.name) + 1):ncol(x2)] <- new.name
+        names(y2)[(ncol(y2) - length(new.name) + 1):ncol(y2)] <- new.name
+    } else {
+        if (any(new.name %in% xcheck) & !elim.old){
+            new.name <- paste0(new.name, ".2")
+            #       names(x)[(ncol(x) - length(new.name) + 1):ncol(x)] <- new.name
+            names(x2)[(ncol(x2) - length(new.name) + 1):ncol(x2)] <- new.name
+            names(y2)[(ncol(y2) - length(new.name) + 1):ncol(y2)] <- new.name
+        }
     }
-    
     p <- data.frame(sapply(x2[, -1], function(x) 
         as.numeric(as.character(x))), check.names=FALSE)
     x2 <- data.frame(x2[, 1, drop=FALSE], p, check.names=FALSE)
     p <- data.frame(sapply(y2[, -1], function(x) 
         as.numeric(as.character(x))), check.names=FALSE)
     y2 <- data.frame(y2[, 1, drop=FALSE], p, check.names=FALSE)
-    
-    d <- as.numeric(gsub("digits=", "", comment(termco.d.object)))
     trnp <- termco.rnp(x2, y2)
-    ####end of fun now lapply it  or single depending in input
     DF <- data.frame(sapply(trnp, Trim), check.names=FALSE)
     DF <- replacer(DF, "0(0)", with = zero.replace)
     DF <- replacer(DF, "0(0.00)", with = zero.replace)
@@ -99,5 +97,7 @@ function(termco.d.object, combined.columns, new.name,
     class(o) <- "termco_c"
     return(o)
 }
+
+    
 
 
--- R/termco.d.R
+++ R/termco.d.R
@@ -61,32 +61,33 @@
 #'   }
 #' 
 termco.d <-
-function(text.var, grouping.var, match.string,
-    ignore.case = FALSE, zero.replace = 0, output = "percent", 
-    digits = 2){
+function (text.var, grouping.var, match.string, ignore.case = FALSE, 
+          zero.replace = 0, output = "percent", digits = 2) {
     NAME <- if (is.list(grouping.var)) {
         m <- unlist(as.character(substitute(grouping.var))[-1])
-        m <- sapply(strsplit(m, "$", fixed=TRUE), function(x) x[length(x)])
-        paste(m, collapse="&")
-    } else {
+        m <- sapply(strsplit(m, "$", fixed = TRUE), function(x) x[length(x)])
+        paste(m, collapse = "&")
+    }
+    else {
         G <- as.character(substitute(grouping.var))
         G[length(G)]
     }
-    x <- termco(text.var = text.var, match.string = match.string,
-        grouping.var = grouping.var, ignore.case = ignore.case)
+    x <- termco(text.var = text.var, match.string = match.string, 
+                grouping.var = grouping.var, ignore.case = ignore.case)
     names(x)[1] <- NAME
     y <- termco.p(tco = x, output = output, digits = digits)
-    if (zero.replace != 0){
-        x[, -c(1:2)] <- lapply(x[, -c(1:2)], 
-            function(x) replacer(x, 0, zero.replace))
-        y[, -c(1:2)] <- lapply(y[, -c(1:2)], 
-            function(x) replacer(x, 0, zero.replace))
+    if (zero.replace != 0) {
+        x[, -c(1:2)] <- lapply(x[, -c(1:2)], function(x) replacer(x, 
+                                                                  0, zero.replace))
+        y[, -c(1:2)] <- lapply(y[, -c(1:2)], function(x) replacer(x, 
+                                                                  0, zero.replace))
     }
     z <- termco.rnp(x, y)
-    h <- paste(zero.replace, "(", zero.replace, ")", sep="")
-    z[, -c(1:2)] <- lapply(z[, -c(1:2)], 
-        function(x) replacer(x, h, zero.replace))
+    h <- paste(zero.replace, "(", zero.replace, ")", sep = "")
+    z[, -c(1:2)] <- lapply(z[, -c(1:2)], function(x) replacer(x, 
+                                                              h, zero.replace))
     o <- list(raw = x, prop = y, rnp = z)
+    comment(o) <- c(output, digits)
     class(o) <- "termco_d"
     return(o)
 }
