259bfaf7e28c95210a34bf26d93837194807dbd6
Tyler Rinker <tyler.rinker@gmail.com>
2012-06-03 21:56:56 -0400

    Removed previous roxygen templates as roxygen does this automatically.

--- R/sentSplit.R
+++ R/sentSplit.R
@@ -1,19 +1,3 @@
-#' Order a data frame by its columns.
-#'
-#' This function completes the subsetting, transforming and ordering triad
-#' with a function that works in a similar way to \code{\link{subset}} and 
-#' \code{\link{transform}} but for reordering a data frame by its columns.
-#' This saves a lot of typing!
-#'
-#' @param df data frame to reorder
-#' @param ... expressions evaluated in the context of \code{df} and 
-#'   then fed to \code{\link{order}}
-#' @keywords manip
-#' @export
-#' @examples
-#' mtcars[with(mtcars, order(cyl, disp)), ]
-#' arrange(mtcars, cyl, disp)
-#' arrange(mtcars, cyl, desc(disp))
 sentSplit <-
 function(dataframe, text.var, splitpoint = NULL, 
     rnames = 'numeric', text.place = 'original') {
