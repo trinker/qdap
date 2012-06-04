8eacd4ed6701add34d14c1f73ee0c23619f1aeb8
Tyler Rinker <tyler.rinker@gmail.com>
2012-06-03 22:57:22 -0400

    added the roxygen header for qda and delte functions

--- R/delete.R
+++ R/delete.R
@@ -1,3 +1,36 @@
+#' Delete Files
+#' 
+#' %% ~~ A concise (1-5 lines) description of what the function does. ~~
+#' 
+#' %% ~~ If necessary, more details than the description above ~~
+#' 
+#' @param file %% ~~Describe \code{file} here~~
+#' @return %% ~Describe the value returned %% If it is a LIST, use %%
+#' \item{comp1 }{Description of 'comp1'} %% \item{comp2 }{Description of
+#' 'comp2'} %% ...
+#' @note %% ~~further notes~~
+#' @author %% ~~who you are~~
+#' @seealso %% ~~objects to See Also as \code{\link{help}}, ~~~
+#' @references %% ~put references to the literature/web site here ~
+#' @keywords ~kwd1 ~kwd2
+#' @examples
+#' 
+#' ##---- Should be DIRECTLY executable !! ----
+#' ##-- ==>  Define data, use random,
+#' ##--	or do  help(data=index)  for the standard data sets.
+#' 
+#' ## The function is currently defined as
+#' function (file = NULL) 
+#' {
+#'     x <- if (is.null(file)) {
+#'         menu(dir())
+#'     }
+#'     else {
+#'         file
+#'     }
+#'     unlink(x, recursive = TRUE, force = FALSE)
+#'   }
+#' 
 delete <-
 function(file = NULL) {
     x <- if (is.null(file)) {
