e2c5e2b8ef65d5ce7593d199c3fa86b0f2da2218
Tyler Rinker <tyler.rinker@gmail.com>
2012-06-08 23:06:51 -0400

    colSplit can now take a NULL argument that willallow for column naming in colsplit2df

--- R/colSplit.R
+++ R/colSplit.R
@@ -40,8 +40,8 @@ function(column, name.sep = "&", col.sep = "."){
     column <- as.data.frame(column)
     svar <- strsplit(as.character(column[, 1]), col.sep, fixed = TRUE)
     svar <- data.frame(do.call('rbind', svar))
-    if (length(unlist(strsplit(names(column), 
-        name.sep, fixed = TRUE))) > 1){
+    if (!is.null(name.sep) & length(unlist(strsplit(names(column), 
+                                                    name.sep, fixed = TRUE))) > 1){
         cn <- strsplit(names(column)[1], name.sep, fixed = TRUE)[[1]]
         names(svar) <- cn
     } 
