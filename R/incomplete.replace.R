984e9fc6925aa6d6e1464470aedd4c25b98f6f50
Tyler Rinker <tyler.rinker@gmail.com>
2012-06-09 23:45:16 -0400

    added incomplete.replace (alias incomp) with en and em dash to incomp sub.  Replacement character is "|"

---
+++ R/incomplete.replace.R
@@ -0,0 +1,15 @@
+incomplete.replace <-
+function(text.var, en.dash = FALSE,
+    em.dash = FALSE) {
+    x <- gsub("[.?!]+\\.[.?!]*|[.?!]*\\.[.?!]+", 
+        "|", scrubber(text.var))
+    if (en.dash) {
+        x <- gsub("[?]", "|", x)
+    }
+    if (em.dash) {
+        x <- gsub("[?]", "|", x)
+    }
+    return(x)
+}
+
+incomp <- incomplete.replace
