881a17d838e10df0490f7594057e33a054fb94e9
tyler rinker <tyler_rinker@hotmail.com>
2012-06-14 14:52:58 -0400

    tweaking for group.var=NULL

--- R/termco.d.R
+++ R/termco.d.R
@@ -80,20 +80,27 @@ function (text.var, grouping.var=NULL, match.string, ignore.case = FALSE,
                 grouping.var = grouping.var, ignore.case = ignore.case)
     names(x)[1] <- NAME
     y <- termco.p(tco = x, output = output, digits = digits)
-    if (zero.replace != 0) {
-      x[, -c(1:2)] <- replacer(x[, -c(1:2)], 0, zero.replace)
-      y[, -c(1:2)] <- replacer(y[, -c(1:2)], 0, zero.replace)
-    }
     z <- termco.rnp(x, y)
     if (is.null(grouping.var)){
-        znull <- as.character(z$DF)
-        names(znull) <- rownames(z)
-        z <- noquote(t(as.data.frame(znull)))
-        rownames(z) <- "all"
+      znull <- as.character(z$DF)
+      names(znull) <- rownames(z)
+      z <- t(as.data.frame(znull))
+      z <- replacer(z, "0(0)", with = zero.replace)
+      z <- replacer(z, "0(0.00)", with = zero.replace)
+      z <- noquote(z)
+      rownames(z) <- "all"
+      if (zero.replace != 0) {
+        x[, -c(1:2)] <- replacer(x[, -c(1:2)], 0, zero.replace)
+        y[, -c(1:2)] <- replacer(y[, -c(1:2)], 0, zero.replace)
+      }
     } else {
-        h <- paste(zero.replace, "(", zero.replace, ")", sep = "")
-        z[, -c(1:2)] <- lapply(z[, -c(1:2)], function(x) replacer(x, 
-            h, zero.replace))
+      if (zero.replace != 0) {
+        x[, -c(1:2)] <- replacer(x[, -c(1:2)], 0, zero.replace)
+        y[, -c(1:2)] <- replacer(y[, -c(1:2)], 0, zero.replace)
+      }
+      h <- paste(zero.replace, "(", zero.replace, ")", sep = "")
+      z[, -c(1:2)] <- lapply(z[, -c(1:2)], function(x) replacer(x, 
+                                                                h, zero.replace))
     }
     o <- list(raw = x, prop = y, rnp = z, zero_replace = zero.replace,
               output = output, digits = digits)
