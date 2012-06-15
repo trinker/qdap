8134cd2bde254cd8a779e775f4c378c15e49720c
tyler rinker <tyler_rinker@hotmail.com>
2012-06-15 18:31:11 -0400

    added adjacency matrix function

---
+++ R/adjacency_matrix.R
@@ -0,0 +1,38 @@
+adjacency_matrix <-
+function(matrix.obj) {
+    if(class(matrix.obj) %in% c("termco_d", "termco_c")){
+        info <- matrix.obj #for later use
+        if (matrix.obj[["zero_replace"]] != 0){
+            matrix.obj <- replacer(matrix.obj[["raw"]], 
+                matrix.obj[["zero_replace"]], 0)
+        } else {
+            matrix.obj <- matrix.obj[["raw"]]
+        }
+        matrix.obj <- termco2mat(matrix.obj)
+    } else {    
+        if (class(matrix.obj) =="matrix") {
+            if (is.null(comment(matrix.obj))){
+                warning("Not a termco or wfm object; results may not be correct.")
+            } else {
+                if(comment(matrix.obj)!="true.matrix"){
+                    warning(paste("Not a termco.d, termco.c or wfm object;",
+                    "results may not be correct."))
+                }
+            }
+        } else {
+             warning("Not a matrix object; results may not be correct.")
+        }
+    }
+    Y <- matrix.obj >= 1
+    Y <- apply(Y, 2, as, "numeric") 
+    rownames(Y) <- rownames(matrix.obj)
+    Z <- Z2 <- t(Y) %*% Y                    
+    Z2[!lower.tri(Z2)] <- NA
+    Z2 <- Z2[-1, -ncol(Z2)]
+    o <- list(boolean = Y, adjacency = Z, shared = Z2, sums = colSums(Y))
+    class(o) <- "adjacency.matrix"
+    return(o)
+}
+
+adjmat <- adjacency_matrix
+
---
+++ R/print.adjacency.matrix.R
@@ -0,0 +1,8 @@
+print.adjacency.matrix <-
+function(adjacency.matrix) {
+    cat("Adjacency Matrix:\n\n")
+    print(adjacency.matrix$shared, na.print="", quote=FALSE)
+    cat("\n\n")
+    cat("Summed Overlap:\n\n")
+    print(adjacency.matrix$sums)
+}
