b28882a72e1ed4fd5365adbd85f6f5a11f225cba
tyler rinker <tyler_rinker@hotmail.com>
2012-06-16 00:05:13 -0400

    added scan.mode option to incomplete replace

--- R/incomplete.replace.R
+++ R/incomplete.replace.R
@@ -1,9 +1,18 @@
 incomplete.replace <-
-function(text.var) {
-    x <- gsub("\\?*\\?[.]+|[.?!]*\\? [.][.?!]+|[.?!]*\\. [.?!]+|
-        [.?!]+\\. [.?!]*|[.?!]+\\.[.?!]*|[.?!]*\\.[.?!]+", "|", 
-        scrubber(text.var))
-    return(x)
+function(text.var, scan.mode = FALSE) {
+  pat <- "\\?*\\?[.]+|[.?!]*\\? [.][.?!]+|[.?!]*\\. [.?!]+|
+        [.?!]+\\. [.?!]*|[.?!]+\\.[.?!]*|[.?!]*\\.[.?!]+"
+    if (scan.mode) {
+      wid <- options()$width
+      options(width = 10000)
+      sel <- grepl(pat, scrubber(text.var))
+      x <- data.frame(row.num = which(sel), 
+                      text = as.character(text.var[sel]))
+      print(left.just(x, 2))
+      options(width = wid)
+    } else {
+      x <- gsub(pat, "|", scrubber(text.var))
+      return(x)
+    }
 }
-
 incomp <- incomplete.replace
