b69f7e9fc494ec749882fba7f0ebf6d37a5fa003
tyler rinker <tyler_rinker@hotmail.com>
2012-06-18 09:31:41 -0400

    added `stem = TRUE` to sentSplit.  Also made stem2df have ... to pass to stemmer

--- R/sentSplit.R
+++ R/sentSplit.R
@@ -92,66 +92,80 @@
 #' 
 sentSplit <-
 function(dataframe, text.var, splitpoint = NULL, incomplete.sub = TRUE,  
-    rnames = 'numeric', text.place = 'original') {
-    DF <- dataframe
-    if (incomplete.sub) {
-        DF[, text.var] <- incomplete.replace(DF[, text.var])
+         stem.col = TRUE, rnames = 'numeric', text.place = 'right', ...) {
+  DF <- dataframe
+  if (is.numeric(text.var)) {
+    text.var <- colnames(DF)[text.var]
+  }
+  if (incomplete.sub) {
+    DF[, text.var] <- incomplete.replace(DF[, text.var])
+  }
+  if(length(DF) < 3) {
+    DF$EXTRA1x2 <-  1:nrow(DF); DF$EXTRA2x2 <-  1:nrow(DF)
+  } else {
+    DF
+  }
+  input <- text.var
+  re <- ifelse(is.null(splitpoint), "[\\?\\.\\!\\|]", 
+               as.character(substitute(splitpoint)))
+  RN <- rnames
+  TP <- text.place
+  breakinput <- function(input, re) {
+    j <- gregexpr(re, input)
+    lengths <- unlist(lapply(j, length))
+    spots <- lapply(j, as.numeric)
+    first <- unlist(lapply(spots, function(x) {
+      c(1, (x + 1)[-length(x)])
     }
-    if(length(DF) < 3) {
-        DF$EXTRA1x2 <-  1:nrow(DF); DF$EXTRA2x2 <-  1:nrow(DF)
+    )
+    )
+    last <- unlist(spots)
+    ans <- substring(rep(input, lengths), first, last)
+    return(list(text = ans, lengths = lengths))
+  }
+  j <- breakinput(DF[, input], re)
+  others <- DF[, -which(colnames(DF) %in% input), drop=FALSE]
+  idx <- rep(1:dim(others)[1], j$lengths)
+  ans <- data.frame(cbind(input = Trim(j$text), others[idx, ]))
+  colnames(ans)[1] <- input
+  x<-as.character(rownames(ans))
+  y<-strsplit(x, "\\.")
+  z<-sapply(y, function(x) as.numeric(x[2]))
+  z[is.na(z)] <- 0
+  z<-as.character(z+1)
+  a<-sapply(y, function(x)x[1])
+  x <-paste(a, ".", z, sep="")
+  rownames(ans) <- TOT <-x
+  if (RN == "numeric") {
+    rownames(ans) <- NULL
+  }
+  if (TP == "original") {
+    ans <- ans[, colnames(DF)]
+    if (stem.col) {
+      ans <- stem2df(ans, which(colnames(ans) %in% text.var), ...)
+    } 
+  } else {
+    if (TP == "right") {
+      ans <- data.frame(ans[, -1], ans[, 1])
+      colnames(ans)<-c(colnames(ans)[-ncol(ans)],input)
+      if (stem.col) {
+        ans <- stem2df(ans, ncol(ans), ...)
+      }   
     } else {
-        DF
+      if (TP == "left") {
+        ans <- ans
+        if (stem.col) {
+          ans <- stem2df(ans, 1, ...)
+        }             
+      } else {
+        warning("incorrect text.place argument")
+      }
     }
-    input <- as.character(substitute(text.var))
-    re <- ifelse(is.null(splitpoint), "[\\?\\.\\!\\|]", 
-        as.character(substitute(splitpoint)))
-    RN <- as.character(substitute(rnames))
-    TP <- as.character(substitute(text.place))
-    TP<-'original'
-    breakinput <- function(input, re) {
-        j <- gregexpr(re, input)
-        lengths <- unlist(lapply(j, length))
-        spots <- lapply(j, as.numeric)
-        first <- unlist(lapply(spots, function(x) {
-                    c(1, (x + 1)[-length(x)])
-                }
-            )
-        )
-        last <- unlist(spots)
-        ans <- substring(rep(input, lengths), first, last)
-        return(list(text = ans, lengths = lengths))
-    }
-    j <- breakinput(DF[, input], re)
-    others <- DF[, -which(colnames(DF) %in% input), drop=FALSE]
-    idx <- rep(1:dim(others)[1], j$lengths)
-    ans <- data.frame(cbind(input = Trim(j$text), others[idx, ]))
-    colnames(ans)[1] <- input
-    x<-as.character(rownames(ans))
-    y<-strsplit(x, "\\.")
-    z<-sapply(y, function(x) as.numeric(x[2]))
-    z[is.na(z)] <- 0
-    z<-as.character(z+1)
-    a<-sapply(y, function(x)x[1])
-    x <-paste(a, ".", z, sep="")
-    rownames(ans) <- TOT <-x
-    if (RN == "numeric") {
-        rownames(ans) <- 1:nrow(ans)
-    }
-    if (TP == "original") {
-        ans <- ans[, colnames(DF)]
-    } else {
-        if (TP == "right") {
-            ans <- data.frame(ans[, -1], ans[, 1])
-            colnames(ans)<-c(colnames(ans)[-ncol(ans)],input)
-        } else {
-            if (TP == "left") {
-                ans
-            }
-        }
-    }
-    if (RN == "numeric") {
-        ans <- data.frame(tot=TOT , ans)
-    }
-    ans$EXTRA1x2 <- NULL; ans$EXTRA2x2 <- NULL
-    return(ans)
+  }
+  if (RN == "numeric") {
+    ans <- data.frame(tot=TOT , ans)
+    ans <- ans[, c(2, 1, 3:ncol(ans))]
+  }
+  ans$EXTRA1x2 <- NULL; ans$EXTRA2x2 <- NULL
+  return(ans)
 }
--- R/stem2df.R
+++ R/stem2df.R
@@ -6,7 +6,7 @@ function (dataframe, text.var, stem.name = NULL, ...) {
     if (is.numeric(dataframe[, text.var])) {
         stop("text.var can not be numeric")
     }
-    new <- stemmer(dataframe[, text.var])
+    new <- stemmer(dataframe[, text.var], ...)
     DF <- data.frame(dataframe, stem.text = new, stringsAsFactors = FALSE)
     if (!is.null(stem.name)) {
         names(DF)[ncol(DF)] <- stem.name
