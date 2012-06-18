1f23cbdae1b1b555137579fbd645d920298da2d5
tyler rinker <tyler_rinker@hotmail.com>
2012-06-17 23:50:49 -0400

    fixed

--- R/htruncDF.R
+++ R/htruncDF.R
@@ -31,7 +31,7 @@
 #' 
 htruncdf <-
 function(dataframe, n=10, width=10) {
-    x <- head(truncDF(dataframe, width),n=n)
+    x <- head(truncdf(dataframe, width),n=n)
     colnames(x) <- as.character(colnames(dataframe))
     return(x)
 }
