862704e172f03e20939b5b139b07db8f28721c0c
tyler rinker <tyler_rinker@hotmail.com>
2012-06-19 02:30:55 -0400

    fixed wordassociate

--- R/print.word_associate.R
+++ R/print.word_associate.R
@@ -1,10 +1,10 @@
-print.word.associate <-
-function(word.associate) {
-    y <- which(substring(names(word.associate), 
+print.word_associate <-
+function(word_associate) {
+    y <- which(substring(names(word_associate), 
         1, 4) == "obs.")
     word.associate2 <- lapply(y, function(x) {
-        rownames(word.associate[[x]]) <- NULL
-        return(word.associate[[x]])
+        rownames(word_associate[[x]]) <- NULL
+        return(word_associate[[x]])
     })
     wid <- options()$width
     options(width = 10000)
