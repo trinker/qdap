9ed203c18e3a818077e4a33659423e69b2a47fd6
tyler rinker <tyler_rinker@hotmail.com>
2012-06-20 08:08:46 -0400

    got rid of sub/char

--- R/folder.R
+++ R/folder.R
@@ -41,7 +41,7 @@ function(folder.name = NULL) {
         SS <- gsub(":", ".", substr(Sys.time(), 1, 19))
         paste(substr(SS, 1, 10), "  Time", substr(SS, 11, 19), sep = "")
     } else {
-        as.character(substitute(folder.name))
+        folder.name
     }
     x <- paste(getwd(), "/", FN, sep = "")
     dir.create(x)
