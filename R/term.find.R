451140f8184e5392361c0b1f80d430c00daa9fbd
tyler rinker <tyler_rinker@hotmail.com>
2012-06-21 19:17:57 -0400

    added a logical to term find

--- R/term.find.R
+++ R/term.find.R
@@ -1,20 +1,29 @@
 term.find <-
-function(str, mat){
-  tester <- function(x, y){
+function(str, mat, logic = FALSE){
+  tester <- function(x, y, logic){
     p <- suppressWarnings(unlist(grepl(x, y, fixed = TRUE)))
-    return(which(p))
+    if (!logic) {
+        p <- which(p)
+    }
+    return(p)
   }
   spacer <- function(string){
     sapply(string, function(x) paste0(" ", x, " "), USE.NAMES = FALSE)
   }
   str <- spacer(str)
-  findit <- function(x) {
-    sort(unique(c(unlist(sapply(x, function(z) tester(z, str))))))
+  if (logic) {
+      findit <- function(x, logic) {
+        sapply(x, function(z) tester(z, str, logic))
+      }
+  } else {
+      findit <- function(x, logic) {
+        sort(unique(c(unlist(sapply(x, function(z) tester(z, str, logic))))))
+      }
   }
   if (is.list(mat)) {
-    a <- lapply(mat, findit)
+    a <- lapply(mat, findit, logic = logic)
   } else {
-    a <- findit(mat)
+    a <- findit(mat, logic = logic)
   }
   return(a)
-} 
+}
