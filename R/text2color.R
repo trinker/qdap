16fe2a39e7fe2a71395cadca87e93d1c6ee9b49e
tyler rinker <tyler_rinker@hotmail.com>
2012-06-23 10:42:28 -0400

    added text2color as an internal function

---
+++ R/text2color.R
@@ -0,0 +1,10 @@
+text2color <-
+function(words, recode.words, colors, nomatch) {
+            lookup <- lapply(seq_along(recode.words), function(n) 
+                cbind(recode.words[[n]], colors[n]))
+            lookup <- do.call("rbind.data.frame", lookup)
+            lookup <- apply(lookup, 2, as.character)
+            recode <- lookup[match(words, lookup[, 1]), 2]
+            recode[is.na(recode)] <- nomatch
+            return(recode)
+        }
