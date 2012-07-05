9808b553adb5d4654b87de332bd3fb177e5209a5
tyler rinker <tyler_rinker@hotmail.com>
2012-07-05 13:09:22 -0400

    fixed key_merge order error

--- R/key_merge.R
+++ R/key_merge.R
@@ -37,12 +37,16 @@
 #' 
 key_merge <-
 function(transcript.df, key.df, common.column, 
-    defualt.arrange = TRUE) {
-    DF <- merge(transcript.df, key.df, by = c(common.column, 
-        common.column), incomparables = NA)
-    if (defualt.arrange) {
-        DF[, c(1, 3:ncol(DF), 2)]
-    } else {
-        DF
-    }
+         defualt.arrange = TRUE) {
+  transcript.df$IDCOL <- 1:nrow(transcript.df)
+  DF <- merge(transcript.df, key.df, by = c(common.column, 
+                                            common.column), incomparables = NA)
+  if (defualt.arrange) {
+    DF <- DF[, c(1, 3:ncol(DF), 2)]
+  } else {
+    DF <- DF
+  }
+  DF <- DF[order(DF$IDCOL), ]
+  DF$IDCOL <- NULL
+  return(DF)
 }
