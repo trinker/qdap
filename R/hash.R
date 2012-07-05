0edd6264664cc42600c33f886f2193baf84beb31
tyler rinker <tyler_rinker@hotmail.com>
2012-07-05 09:02:06 -0400

    added format (indent)

--- R/hash.R
+++ R/hash.R
@@ -33,6 +33,6 @@ function(x) {
     e <- new.env(hash = TRUE, size = nrow(x), 
         parent = emptyenv())
     apply(x, 1, function(col) assign(col[1], 
-    as.numeric(col[2]), envir = e))
+        as.numeric(col[2]), envir = e))
     return(e)
 }
