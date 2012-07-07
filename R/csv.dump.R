112b2c8e01ed62bd1666bd0f6e1a6c71c2a93718
tyler rinker <tyler_rinker@hotmail.com>
2012-07-06 22:40:05 -0400

    fixed naming bug

--- R/csv.dump.R
+++ R/csv.dump.R
@@ -46,22 +46,24 @@
 #' 
 csv.dump <-
 function(..., dir = NULL, open = FALSE){
-    x <- match.call(expand.dots = FALSE)
-    z <- as.character(x[[2]])
-    x2 <- list(...)
-    names(x2) <- z
-    y <- qdap::folder(folder.name = dir)
-    files <- paste0(y, "/", z, ".csv")
-    lapply(seq_along(x2), function(i){
-        write.table(x2[i], file = files[i],  sep = ",", col.names = T, 
-        row.names=F, qmethod = "double")
-        }
-    )
-    if(open){
-        if (.Platform['OS.type'] == "windows"){
-            shell.exec(y)
-        } else {
-            system(paste(Sys.getenv("R_BROWSER"), y))
-        }
+  x <- match.call(expand.dots = FALSE)
+  z <- as.character(x[[2]])
+  x2 <- list(...)
+  names(x2) <- z
+  y <- qdap::folder(folder.name = dir)
+  files <- paste0(y, "/", z, ".csv")
+  lapply(seq_along(x2), function(i){
+    x3 <-x2[i]
+    names(x3) <- gsub(names(x2)[i], "", names(x3))
+    write.table(x3, file = files[i],  sep = ",", col.names = TRUE, 
+                row.names=F, qmethod = "double")
+  }
+  )
+  if(open){
+    if (.Platform['OS.type'] == "windows"){
+      shell.exec(y)
+    } else {
+      system(paste(Sys.getenv("R_BROWSER"), y))
     }
+  }
 }
