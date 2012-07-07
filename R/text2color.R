3196a09db3a52c95a26538f77efd7852dc0a4ecf
tyler rinker <tyler_rinker@hotmail.com>
2012-07-07 18:16:16 -0400

    bug fix

--- R/text2color.R
+++ R/text2color.R
@@ -1,13 +1,16 @@
 text2color <-
 function(words, recode.words, colors) {
-    nc <- length(colors)
-    nomatch <- colors[nc]
-    colors <- colors[-nc]
-    lookup <- lapply(seq_along(recode.words), function(n) 
-        cbind(recode.words[[n]], colors[n]))
-    lookup <- do.call("rbind.data.frame", lookup)
-    lookup <- apply(lookup, 2, as.character)
-    recode <- lookup[match(words, lookup[, 1]), 2]
-       recode[is.na(recode)] <- nomatch
-    return(recode)
+  nc <- length(colors)
+  nomatch <- colors[nc]
+  colors <- colors[-nc]
+  nulls <- sapply(recode.words, function(x) identical(x, character(0)))
+  recode.words[nulls] <- ""
+  lookup <- lapply(seq_along(recode.words), function(n) 
+    cbind(recode.words[[n]], colors[n]))
+  lookup <- do.call("rbind.data.frame", lookup)
+  lookup <- apply(lookup, 2, as.character)
+  recode <- lookup[match(words, lookup[, 1]), 2]
+  recode[is.na(recode)] <- nomatch
+  return(recode)
 }
+
