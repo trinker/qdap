69a10974e48fb644f43ff1adb976ffdef5ae1f94
tyler rinker <tyler_rinker@hotmail.com>
2012-07-07 16:00:39 -0400

    added some arguments to wfm

--- R/word.freq.matrix.R
+++ R/word.freq.matrix.R
@@ -62,34 +62,34 @@
 #' 
 word.freq.matrix <-
 function(text.var = NULL, grouping.var = NULL, wfdf = NULL,
-    stopwords = NULL, digits = 2){
-    if (!is.null(wfdf)) {
-        if (comment(wfdf) == "t.df") {
-            wfdf <- wfdf
-        } else {
-            if (comment(wfdf) == "m.df") { 
-                wfdf <- wfdf[-nrow(wfdf), -ncol(wfdf)]
-            } else {
-                stop("Object must be a raw word frequency data frame")
-            }
-        }
-
-        x2 <- wfdf[, -1, drop = FALSE]
-        rownames(x2) <- wfdf[, 1]
-        x2 <- as.matrix(x2)
+         output = "raw", stopwords = NULL, digits = 2){
+  if (!is.null(wfdf)) {
+    if (comment(wfdf) == "t.df") {
+      wfdf <- wfdf
     } else {
-        if (!is.null(text.var) & ! is.null(grouping.var)) {
-            wfdf <- word.freq.df(text.var = text.var, grouping.var = grouping.var, 
-                 stopwords = stopwords) 
-            x2 <- wfdf[, -1, drop = FALSE]
-            rownames(x2) <- wfdf[, 1]
-            x2 <- as.matrix(x2)
-        } else {
-            stop ("must specify both text.var & grouping var or wfdf")
-        }
+      if (comment(wfdf) == "m.df") { 
+        wfdf <- wfdf[-nrow(wfdf), -ncol(wfdf)]
+      } else {
+        stop("Object must be a raw word frequency data frame")
+      }
     }
-    comment(x2) <- "true.matrix"
-    return(x2)
+    
+    x2 <- wfdf[, -1, drop = FALSE]
+    rownames(x2) <- wfdf[, 1]
+    x2 <- as.matrix(x2)
+  } else {
+    if (!is.null(text.var)) {
+      wfdf <- word.freq.df(text.var = text.var, grouping.var = grouping.var, 
+                           stopwords = stopwords, output = output, digits = digits) 
+      x2 <- wfdf[, -1, drop = FALSE]
+      rownames(x2) <- wfdf[, 1]
+      x2 <- as.matrix(x2)
+    } else {
+      stop ("must specify both text.var & grouping var or wfdf")
+    }
+  }
+  comment(x2) <- "true.matrix"
+  return(x2)
 }
 
-wfm <- word.freq.matrix 
+wfm <- word.freq.matrix
