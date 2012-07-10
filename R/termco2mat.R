707eb9c0729a429630e831a233f41a47538ae93e
tyler rinker <tyler_rinker@hotmail.com>
2012-07-10 08:01:14 -0400

    takes termco_c or termco_d objects now

--- R/termco2mat.R
+++ R/termco2mat.R
@@ -18,39 +18,41 @@
 #' ##-- ==>  Define data, use random,
 #' ##--	or do  help(data=index)  for the standard data sets.
 #' 
-termco2mat <-
-function (dataframe, drop.wc = TRUE, short.colnames = TRUE, rm.zerocol = FALSE, 
-    no.quote = TRUE, transform = TRUE) {
-    if (!is.data.frame(dataframe)) {
-        stop("Please supply a data.frame to termco2mat")
-    }
-    ind <- if (drop.wc) {
-        1:2
-    } else {
-        1
-    }
-    MAT <- as.matrix(dataframe[, -c(ind)])
-    rownames(MAT) <- dataframe[, 1]
-    if (short.colnames) {
-        mn <- gsub("(.*)\\)([^\\)]*)", "\\1\\2", colnames(MAT))
-        colnames(MAT) <- gsub("term(", "", mn, fixed=TRUE)
-    }
-    if (rm.zerocol) {
-        fun <- function(x) all(ifelse(x == 0, T, F))
-        MAT <- MAT[, !apply(MAT, 2, fun)]
-    }
-    
-    OC <- length(grep("(", as.vector(unlist(MAT)), fixed = TRUE)) == 0
-    if (OC) {
-        z <- rownames(MAT)
-        MAT <- apply(MAT, 2, as.numeric)
-        rownames(MAT) <- z
-    }
-    if (no.quote & !OC){ 
-        MAT <- noquote(MAT)
-    }
-    if (transform){
-        MAT <- t(MAT)
-    }
-    return(MAT)
+termco2mat <-function (dataframe, drop.wc = TRUE, short.colnames = TRUE, 
+  rm.zerocol = FALSE, no.quote = TRUE, transform = TRUE) {
+  if (class(dataframe) %in% c("termco_d", "termco_c")) {
+    dataframe <- dataframe[["raw"]]
+  }
+  if (!is.data.frame(dataframe)) {
+    stop("Please supply a data.frame to termco2mat")
+  }
+  ind <- if (drop.wc) {
+    1:2
+  } else {
+    1
+  }
+  MAT <- as.matrix(dataframe[, -c(ind)])
+  rownames(MAT) <- dataframe[, 1]
+  if (short.colnames) {
+    mn <- gsub("(.*)\\)([^\\)]*)", "\\1\\2", colnames(MAT))
+    colnames(MAT) <- gsub("term(", "", mn, fixed=TRUE)
+  }
+  if (rm.zerocol) {
+    fun <- function(x) all(ifelse(x == 0, T, F))
+    MAT <- MAT[, !apply(MAT, 2, fun)]
+  }
+  
+  OC <- length(grep("(", as.vector(unlist(MAT)), fixed = TRUE)) == 0
+  if (OC) {
+    z <- rownames(MAT)
+    MAT <- apply(MAT, 2, as.numeric)
+    rownames(MAT) <- z
+  }
+  if (no.quote & !OC){ 
+    MAT <- noquote(MAT)
+  }
+  if (transform){
+    MAT <- t(MAT)
+  }
+  return(MAT)
 }
