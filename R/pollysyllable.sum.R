9e48e4b7cc1f32b129889bd89eeec0cce3d9514f
tyler rinker <tyler_rinker@hotmail.com>
2012-07-11 23:08:10 -0400

    fixed non character bug

--- R/pollysyllable.sum.R
+++ R/pollysyllable.sum.R
@@ -36,7 +36,11 @@ pollysyllable.sum <-
 function(text) {
     Trim <- function(x) gsub("^\\s+|\\s+$", "", x)
     counter <- function(x) {
-        y <- as.data.frame(table(syllable.count(Trim(x))["syllables"]))
+      v <- table(syllable.count(Trim(x))["syllables"])
+        if (identical(c(v), integer(0))){
+          return(0)
+        }
+        y <- as.data.frame(v))
         z <- subset(y, as.numeric(as.character(Var1)) >= 3)
         j <- sum(z$Freq)
         return(j)
