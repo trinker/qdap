48bb6a1b18c0283df015215f7c60cbd83eec0903
tyler rinker <tyler_rinker@hotmail.com>
2012-07-15 15:23:27 -0400

    fixed class

--- R/pos.by.R
+++ R/pos.by.R
@@ -111,7 +111,6 @@
 pos.by <-
 function(text.var, grouping.var = NULL, digits = 2, 
     return.pos = FALSE){
-    COM <- comment(text.var)
     G <- if(is.null(grouping.var)) {
              "all"
          } else {
@@ -125,42 +124,13 @@ function(text.var, grouping.var = NULL, digits = 2,
                   G[length(G)]
              }
          }
-    if (is.null(comment(text.var))){
+    if (!class(text.var) %in% c("POS", "POSby", "formality.measure")) {
         pos.list <- pos(text.var, digits = digits)
-        text.var <- pos.list[[3]]
+        text.var <- pos.list[["POSfreq"]]
     } else {
-        com <- comment(text.var)
-        text.var <- switch(com,
-            POS = text.var[[3]],
-            POStagged = {
-                o <- text.var[, 2]
-                lev <- sort(unique(unlist(o)))
-                G4 <- do.call(rbind,lapply(o, function(x,lev){ 
-                            tabulate(factor(x,levels = lev,
-                            ordered = TRUE),nbins = length(lev))
-                        },
-                    lev = lev))
-                colnames(G4) <-sort(lev)
-                G4 <- data.frame(wrd.cnt = text.var[, 3], G4, 
-                    check.names = FALSE)
-                    if (any(is.na(G4$wrd.cnt))) {
-                        nas <- which(is.na(G4$wrd.cnt))
-                        G4[nas, 2:ncol(G4)] <- NA
-                    }
-                G4
-            },
-        POSprop = {
-                text.var <- data.frame(text.var[, 1], 
-                    round(sapply(text.var[, -1], 
-                    function(x) x*text.var[, 1])), 
-                    check.names = FALSE)
-                names(text.var) <- gsub("prop", "", names(text.var))
-                names(text.var)[1] <- "wrd.cnt"
-                text.var
-            }, 
-        POSfreq = text.var
-        )
-    }    
+        pos.list <- text.var
+        text.var <- text.var[["POSfreq"]]
+    }
     grouping <- if(is.null(grouping.var)){
                      rep("all", nrow(text.var))
                  } else {
@@ -181,16 +151,12 @@ function(text.var, grouping.var = NULL, digits = 2,
     DF2 <- data.frame(replace = rownames(DF2), DF2, check.names = FALSE)
     rownames(DF2) <- 1:nrow(DF2)
     colnames(DF2)[1] <- G
-    if (is.null(COM) & return.pos) {
-        return(list("p" = pos.list, "outcome" = DF2))
-    } else {
-        return(DF2)
-    }
-    if (!is.null(COM)){
-        if (COM=="POSprop"){
-            W1 <- "Supplied text.var is based on proportions."  
-            W2 <- "  Outcomes may be inaccurate."
-            warning(paste0(W1, W2))
-        }
-    }
+    o <- unclass(pos.list)
+    o[["pos.by.freq"]] <- DF2
+    propby <- lapply(1:nrow(DF2), function(i) DF2[i, -1]/rowSums(DF2[, -1])[i])
+    propby <- sapply(do.call(rbind, propby), round, digits = digits)
+    propby[is.nan(propby)] <- 0
+    o[["pos.by.prop"]] <- data.frame(DF2[, 1, drop=FALSE], propby)
+    class(o) <- "POSby"
+    return(o)
 }
