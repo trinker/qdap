238b54a42b991f0455bbe3a94dfc1da32172f87b
tyler rinker <tyler_rinker@hotmail.com>
2012-07-14 23:29:06 -0400

    tweaked formality and fixed math errors

--- R/formality.R
+++ R/formality.R
@@ -1,8 +1,4 @@
-formality <-
-function(text.var, grouping.var = NULL, sort.by.formality = TRUE){
-    x <- "\n\nHeylighen, F., & Dewaele, J.-M. (2002). Variation in the contextuality of language: an
-        empirical measure. Context in Context, Special issue of Foundations of Science, 7 (3),
-        293–340.\n\n\n\n\n"
+formality <- function(text.var, grouping.var = NULL, sort.by.formality = TRUE){
     G <- if(is.null(grouping.var)) {
              "all"
          } else {
@@ -21,21 +17,42 @@ function(text.var, grouping.var = NULL, sort.by.formality = TRUE){
     xn <- nrow(X)
     X$JI <- rep(0, xn)
     X$JK <- rep(0, xn)
-    DF1 <- data.frame( 
-        noun = rowSums(X[, names(X) %in% c('NN', 'NNS', 'NNP', 'NNPS', 'POS', 'JI', 'JK')]), 
-        verb = rowSums(X[, names(X) %in% c('MD', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ', 'JI', 'JK')]),  
-        adverb = rowSums(X[, names(X) %in% c('RB', 'RBR', 'RBS', 'WRB', 'JI', 'JK')]),  
-        pronoun = rowSums(X[, names(X) %in% c('PRP', 'PRP.', 'WDT', 'WP', 'WP.', 'JI', 'JK')]),  
-        prep = rowSums(X[, names(X) %in% c('IN', 'RP', 'TO', 'JI', 'JK')]),  
-        adj = rowSums(X[, names(X) %in% c('CD', 'DT', 'JJ', 'JJR', 'JJS', 'JI', 'JK')]),  
-        interj = rowSums(X[, names(X) %in% c('UH', 'JI', 'JK')]))
-
-    FOR <- (rowSums(cbind(DF1$noun, DF1$adj, DF1$prep)) - rowSums(cbind(DF1$pronoun, 
-        DF1$verb, DF1$adverb, DF1$interj)) + 100)/2
-    if(!is.null(grouping.var)) {
-        FOR <- data.frame(replace = X[, 1], formality = FOR); colnames(FOR)[1] <- G
+    article <- function(x) {
+        WORDS <- stopwords(x, stopwords = NULL, 
+            unlist = FALSE, strip = TRUE)
+        sapply(WORDS, function(x) sum(x %in% c("the", "an", "a")))
     }
-    if (sort.by.formality) {
+    if (!is.null(grouping.var)){
+        articles <- unlist(lapply(split(text.var, grouping.var), function(x) sum(article(x))))
+    } else {
+        articles <- sum(article(text.var))
+    }
+    if (!is.null(X$DT)) {
+        PD <- X$DT-articles
+    }
+    DF1 <- data.frame( 
+        noun = rowSums(X[, names(X) %in% c("NN", "NNS", "NNP", "NNPS", 
+            "POS", "JI", "JK")]), 
+        articles = articles,
+        verb = rowSums(X[, names(X) %in% c("MD", "VB", "VBD", "VBG", 
+            "VBN", "VBP", "VBZ", "JI", "JK")]),  
+        adverb = rowSums(X[, names(X) %in% c("RB", "RBR", "RBS", "WRB", 
+            "JI", "JK")]),  
+        pronoun = rowSums(X[, names(X) %in% c("PRP", "PRP.", "WDT", "WP", 
+            "WP.", "JI", "JK", "EX")]),  
+        prep = rowSums(X[, names(X) %in% c("IN", "RP", "TO", "JI", "JK")]),  
+        adj = rowSums(cbind(X[, names(X) %in% c("CD", "JJ", "JJR", "JJS", 
+            "JI", "JK")], PD)),  
+        interj = rowSums(X[, names(X) %in% c("UH", "JI", "JK")]))
+    DF1RS <- rowSums(DF1)
+    DF1 <- data.frame(apply(DF1, 2, function(x) 100*(x/DF1RS)))
+    FOR <- (rowSums(cbind(DF1$noun, DF1$article, DF1$adj, DF1$prep)) - 
+        rowSums(cbind(DF1$pronoun, DF1$verb, DF1$adverb, DF1$interj)) + 100)/2
+    if(!is.null(grouping.var)) {
+        FOR <- data.frame(replace = X[, 1], formality = FOR)
+        colnames(FOR)[1] <- G
+    }
+    if (!is.null(grouping.var) & sort.by.formality) {
         FOR <- FOR[order(-FOR$formality), ]
         rownames(FOR) <- NULL
     }
