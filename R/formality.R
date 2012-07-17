ca9cb54727553fec9aa7d5c0a8a58e1fdd12defe
tyler rinker <tyler_rinker@hotmail.com>
2012-07-17 00:41:08 -0400

    fix bug in plot

--- R/formality.R
+++ R/formality.R
@@ -89,11 +89,12 @@ formality <- function(text.var, grouping.var = NULL, plot = FALSE,
         FOR <- FOR[order(-FOR$formality), ]
         rownames(FOR) <- NULL
     }
-    prop.by <- data.frame(FOR[, 1, drop = FALSE], 
-        word.count = WOR[match(FOR[, 1], names(WOR))], 
+    prop.by <- data.frame(var=names(WOR), 
+        word.count = WOR, 
         apply(DF1, 2, round, digits = digits))
-    freq.by <- data.frame(FOR[, 1, drop = FALSE], 
-        word.count = WOR[match(FOR[, 1], names(WOR))], DF2)
+    freq.by <- data.frame(var=names(WOR), 
+        word.count = WOR, DF2)
+    colnames(prop.by)[1] <- colnames(freq.by)[1] <- colnames(FOR)[1]
     rownames(prop.by) <- rownames(freq.by) <- NULL
     o <- unclass(pos.list)
     o$form.freq.by <- freq.by
@@ -111,7 +112,9 @@ formality <- function(text.var, grouping.var = NULL, plot = FALSE,
     row.names(dat) <- NULL
     o$pos.reshaped <- dat
     o$formality <- FOR
+browser()
     if (plot) {
+browser()
         suppressWarnings(require(ggplot2))
         suppressWarnings(require(gridExtra))
         YY <- ggplot(dat, aes(grouping,  fill=form.class)) + 
