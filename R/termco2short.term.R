858b1dddbf629347062fa606305f09ac1253446e
tyler rinker <tyler_rinker@hotmail.com>
2012-07-21 11:17:01 -0400

    changed name one more time

---
+++ R/termco2short.term.R
@@ -0,0 +1,19 @@
+termco2short.term <- 
+function(termco.object, trim.terms = TRUE){
+  CLASS <- class(termco.object)
+  short.name <- function(df){
+    mn <- gsub("(.*)\\)([^\\)]*)", "\\1\\2", colnames(df))
+    colnames(df) <- Trim(gsub("term(", "", mn, fixed=TRUE))
+    return(df)
+  }
+  o <- lapply(termco.object, function(x){
+      if (is.data.frame(x)){
+        short.name(x)
+      } else {
+        x
+      }
+    }
+  )
+  class(o) <- CLASS
+  return(o)
+}
