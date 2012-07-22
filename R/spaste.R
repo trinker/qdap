22922c87dd6a80955b4e2755e738bcee3d147fd0
tyler rinker <tyler_rinker@hotmail.com>
2012-07-21 23:51:44 -0400

    spaste can work on lists now

--- R/spaste.R
+++ R/spaste.R
@@ -1,4 +1,13 @@
-spaste <-
-function(terms){
-    return(paste0(" ", terms, " "))
+spaste <- 
+function(terms, left = FALSE){
+  pas <- function(x) paste0(" ", x, " ")
+  if (left) {
+    pas <- function(x) paste0(" ", x)
+  }
+  if (is.list(terms)) {
+    z <- lapply(terms, pas)
+  } else {
+    z <- pas(terms)
+  }
+  return(z)
 }
