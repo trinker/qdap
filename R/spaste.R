eff5f41b57540e914a101953a2a65dde01d2dc33
tyler rinker <tyler_rinker@hotmail.com>
2012-07-22 11:40:18 -0400

    added args leading and trailing

--- R/spaste.R
+++ R/spaste.R
@@ -1,9 +1,16 @@
 spaste <- 
-function(terms, left = FALSE){
-  pas <- function(x) paste0(" ", x, " ")
-  if (left) {
-    pas <- function(x) paste0(" ", x)
+function(terms, leading = TRUE, trailing = TRUE){
+  if (leading) {
+    s1 <- " "
+  } else {
+    s1 <- ""
   }
+  if (trailing) {
+    s2 <- " "
+  } else {
+    s2 <- ""
+  }
+  pas <- function(x) paste0(s1, x, s2)
   if (is.list(terms)) {
     z <- lapply(terms, pas)
   } else {
