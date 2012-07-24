fce4fb5acd0e396d9736929589f7d1ccccc6f701
tyler rinker <tyler_rinker@hotmail.com>
2012-07-24 15:59:54 -0400

    added some tweaks with spaste to qcv

--- R/qcv.R
+++ R/qcv.R
@@ -1,9 +1,10 @@
 qcv <- 
-function(..., space.wrap = FALSE, sep = ",", fixed = TRUE){
+function(..., space.wrap = FALSE, trailing = FALSE, leading = FALSE){
   x <- substitute(...())
   z <- Trim(unlist(lapply(x, function(y) as.character(y)))  )
   if (space.wrap){
     z <- spaste(z)
   }      
+  z <- spaste(z, trailing = trailing, leading = leading) 
   return(z)
 }
