859b0e221e333ef64ed913554a190c83f3de801b
tyler rinker <tyler_rinker@hotmail.com>
2012-07-24 21:51:45 -0400

    added a terms argument for odd character vectors

--- R/qcv.R
+++ R/qcv.R
@@ -1,6 +1,11 @@
 qcv <- 
-function(..., space.wrap = FALSE, trailing = FALSE, leading = FALSE){
-  x <- substitute(...())
+  function(..., terms = NULL, space.wrap = FALSE, trailing = FALSE, 
+    leading = FALSE){
+  if (!is.null(terms)) {
+    x <- strsplit(terms, split=",")
+  } else {
+    x <- substitute(...())
+  }
   z <- Trim(unlist(lapply(x, function(y) as.character(y))))
   if (space.wrap){
     z <- spaste(z)
