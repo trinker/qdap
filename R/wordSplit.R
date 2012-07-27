1842f0d1129a4c1d4ee73c3436ee499f91a6ee28
tyler rinker <tyler_rinker@hotmail.com>
2012-07-27 12:26:36 -0400

    added the wordSplit function

---
+++ R/wordSplit.R
@@ -0,0 +1,30 @@
+wordSplit <-
+function(dataframe, text.var, codes = NULL, csv = FALSE, 
+    file.name = NULL, strip =FALSE){
+    tv <- as.character(dataframe[, text.var])
+    if (strip) {
+        tv <- strip(tv)
+    }
+    wrds <- lapply(tv, function(x) Trim(unlist(strsplit(x, " "))))
+    lens <- sapply(wrds, length) 
+    leftover <- dataframe[, !colnames(dataframe) %in% text.var, drop =FALSE]
+    if (!is.null(codes)) {
+        lcodes <- length(codes)
+        lwrds <- length(unlist(wrds))
+        MAT <- matrix(rep(0, lcodes*lwrds), lwrds, lcodes)
+        colnames(MAT) <- codes      
+        DF <- data.frame(leftover[rep(1:nrow(leftover), lens), , 
+            drop = FALSE], text=unlist(wrds), MAT)  
+    } else {
+        DF <- data.frame(leftover[rep(1:nrow(leftover), lens), , 
+            drop = FALSE], text=unlist(wrds)) 
+    }
+    if(csv) {
+        if (is.null(file.name)) {
+            file.name <- as.character(substitute(dataframe))
+        } 
+        write.table(DF, file = paste0(file.name, ".csv"),  sep = ",", 
+            col.names = T, row.names=F, qmethod = "double") 
+    }
+    return(DF)
+}
