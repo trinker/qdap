def50acddc263773442cc56398b937450d1383c0
tyler rinker <tyler_rinker@hotmail.com>
2012-07-28 16:05:32 -0400

    added code2long function

---
+++ R/code2long.R
@@ -0,0 +1,21 @@
+code2long <-
+function(dataframe, code.vars, repeat.vars = NULL){
+    if (is.numeric(code.vars)) {
+        code.vars <- colnames(dataframe)[code.vars]
+    }
+    A <- paste2(dataframe[, code.vars])
+    B <- lapply(strsplit(A, "\\."), function(x) as.logical(as.numeric(x)))
+    D <- lapply(B, function(x) code.vars[x])
+    if (is.null(repeat.vars)){
+        repeat.vars <- colnames(dataframe)[!c(text.var, codes) %in% colnames(dataframe)]
+    } else {
+        if (is.numeric(repeat.vars)) {
+            repeat.vars <- colnames(dataframe)[repeat.vars]
+        }
+    }
+    E <- dat[, repeat.vars, drop = FALSE]
+    lens <- sapply(D, length)
+    NEW <- data.frame(code = unlist(D), E[rep(1:nrow(E), lens), ])
+    rownames(NEW) <- NULL
+    return(NEW)
+}
