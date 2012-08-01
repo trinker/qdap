b10ce3ccf504d78f780d83953158d9b4deb05ba6
tyler rinker <tyler_rinker@hotmail.com>
2012-08-01 08:10:22 -0400

    made it work when the replace is a factor

--- R/lookup.R
+++ R/lookup.R
@@ -1,6 +1,5 @@
 lookup <-
 function(terms, key.match, key.reassign=NULL, missing = NA) {
-
     hash <- function(x, mode.out) {
         e <- new.env(hash = TRUE, size = nrow(x), 
             parent = emptyenv())
@@ -11,12 +10,19 @@ function(terms, key.match, key.reassign=NULL, missing = NA) {
         return(e)
     }  
     if (is.null(key.reassign)) {
+        if (is.factor(key.match[, 2])) {
+            key.match[, 2] <- as.character(key.match[, 2])
+        }
         mode.out <- mode(key.match[, 2])    
         DF <- key.match
         DF[, 1] <- as.character(DF[, 1])
     } else {
+        if (is.factor(key.reassign)) {
+            key.reassign <- as.character(key.reassign)
+        }        
         mode.out <- mode(key.reassign)    
-        DF <- data.frame(as.character(key.match), key.reassign)   
+        DF <- data.frame(as.character(key.match), key.reassign, 
+            stringsAsFactors = FALSE)   
     }
     KEY <- hash(DF, mode.out = mode.out)                                                               
     recoder <- function(x, env, missing){                               
