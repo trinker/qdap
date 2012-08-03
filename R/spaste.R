52e0b7d58dc1c4a05a84706e7ba21e50661368b4
tyler rinker <tyler_rinker@hotmail.com>
2012-08-03 16:36:19 -0400

    rem.duplicates2 namespace

--- R/spaste.R
+++ R/spaste.R
@@ -1,20 +1,20 @@
 spaste <- 
 function(terms, trailing = TRUE, leading = TRUE){
-  if (leading) {
-    s1 <- " "
-  } else {
-    s1 <- ""
-  }
-  if (trailing) {
-    s2 <- " "
-  } else {
-    s2 <- ""
-  }
-  pas <- function(x) paste0(s1, x, s2)
-  if (is.list(terms)) {
-    z <- lapply(terms, pas)
-  } else {
-    z <- pas(terms)
-  }
-  return(z)
+    if (leading) {
+        s1 <- " "
+    } else {
+        s1 <- ""
+    }
+    if (trailing) {
+        s2 <- " "
+    } else {
+        s2 <- ""
+    }
+    pas <- function(x) paste0(s1, x, s2)
+    if (is.list(terms)) {
+        z <- lapply(terms, pas)
+    } else {
+        z <- pas(terms)
+    }
+    return(z)
 }
