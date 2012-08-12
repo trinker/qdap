1dc313969f3f258635220b3023fd0536decda4aa
tyler rinker <tyler_rinker@hotmail.com>
2012-08-12 08:24:03 -0400

    added documentation for all.words

--- R/all.words.R
+++ R/all.words.R
@@ -1,61 +1,25 @@
 #' Searches Text Column for Words
 #' 
-#' %% ~~ A concise (1-5 lines) description of what the function does. ~~
+#' A convenience function to find words that begin with or contain a letter chunk and returns the frequency counts of the number of occurrences of each word.
 #' 
-#' %% ~~ If necessary, more details than the description above ~~
-#' 
-#' @param text.var %% ~~Describe \code{text.var} here~~
-#' @param begins.with %% ~~Describe \code{begins.with} here~~
-#' @param contains %% ~~Describe \code{contains} here~~
-#' @return %% ~Describe the value returned %% If it is a LIST, use %%
-#' \item{comp1 }{Description of 'comp1'} %% \item{comp2 }{Description of
-#' 'comp2'} %% ...
-#' @note %% ~~further notes~~
-#' @author %% ~~who you are~~
-#' @seealso %% ~~objects to See Also as \code{\link{help}}, ~~~
-#' @references %% ~put references to the literature/web site here ~
-#' @keywords ~kwd1 ~kwd2
+#' @param text.var The text variable
+#' @param begins.with This argument takes a word chunk.  Default is NULL. Use this if searching for a word begining with the word chunk.
+#' @param contains This argument takes a word chunk.  Default is NULL. Use this if searching for a word containing the word chunk.
+#' @return Returns a dataframe with frequency counts of words that begin with or containt he provided word chunk.
+#' @note Can not provide both begins.with and contains arguments at once.  If both begins.with and contains are NULL all.words returns a frequency count for all words.
+#' @seealso 
+#' \code{\link[qdap]{term.match}}
 #' @examples
-#' 
-#' ##---- Should be DIRECTLY executable !! ----
-#' ##-- ==>  Define data, use random,
-#' ##--	or do  help(data=index)  for the standard data sets.
-#' 
-#' ## The function is currently defined as
-#' function (text.var, begins.with = NULL, contains = NULL) 
-#' {
-#'     if (!is.null(begins.with) & !is.null(contains)) {
-#'         stop("Can not use both 'begins.with' & 'contains' arguments")
-#'     }
-#'     if (!is.null(begins.with)) 
-#'         begins.with <- tolower(begins.with)
-#'     if (!is.null(contains)) 
-#'         contains <- tolower(contains)
-#'     WORDS <- unlist(word.split(reducer(strip(text.var))))
-#'     names(WORDS) <- NULL
-#'     y <- data.frame(table(WORDS), stringsAsFactors = FALSE)
-#'     names(y) <- c("WORD", "FREQ")
-#'     y$WORD <- as.character(y$WORD)
-#'     if (!is.null(begins.with)) {
-#'         y <- y[substring(y[, 1], 1, nchar(begins.with)) %in% 
-#'             begins.with, ]
-#'         if (nrow(y) == 0) 
-#'             stop("No words match")
-#'     }
-#'     if (!is.null(contains)) {
-#'         y <- y[grep(contains, y[, 1]), ]
-#'         if (nrow(y) == 0) 
-#'             stop("No words match")
-#'     }
-#'     left.just(y, "WORD")
-#'   }
+#' #' all.words(raj$dialogue, begins.with="re")
+#' all.words(raj$dialogue, "q")
+#' all.words(raj$dialogue, contains="conc")
+#' all.words(raj$dialogue)
 #' 
 all.words <-
 function(text.var, begins.with = NULL, contains = NULL){
     if (!is.null(begins.with) & !is.null(contains)) {
         stop("Can not use both 'begins.with' & 'contains' arguments")
     }
-
     if(!is.null(begins.with)) begins.with <- tolower(begins.with)
     if(!is.null(contains)) contains <- tolower(contains)
     WORDS <- unlist(word.split(reducer(strip(text.var))))
@@ -67,10 +31,10 @@ function(text.var, begins.with = NULL, contains = NULL){
         y <- y[substring(y[, 1], 1, nchar(begins.with)) %in% begins.with, ]
         if(nrow(y)==0) stop("No words match")
     }
-
     if (!is.null(contains)) {
         y <- y[grep(contains, y[, 1]), ]
         if(nrow(y)==0) stop("No words match")
     }
+    rownames(y) <- NULL
     left.just(y, "WORD")
 }
