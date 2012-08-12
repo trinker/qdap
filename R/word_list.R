6e014c58a71904a6134aff38b1c4d409ead0c123
tyler rinker <tyler_rinker@hotmail.com>
2012-08-12 14:52:11 -0400

    fixed issue 50 (reverse alphabetical order)

--- R/word_list.R
+++ R/word_list.R
@@ -24,160 +24,160 @@
 #' 
 word_list <- 
 function(text.var, grouping.var = NULL, stopwords = NULL, alphabetical = FALSE,
-         cut.n = 20, cap = TRUE, cap.list=NULL, cap.I=TRUE, rm.bracket = TRUE) {
-  upper <- function(x) paste(substring(x, 1, 1), 
-                             substring(x, 2, nchar(x)), sep="")
-  Sw1 <- stopwords[!substring(stopwords, 1, 1) %in% LETTERS]
-  Sw2 <- stopwords[substring(stopwords, 1, 1) %in% LETTERS]
-  Sw3 <- if (!is.null(Sw2)) {
-    paste0(Sw2, "'s")
-  } else {
-    NULL
-  }
-  stopwords <- c(Sw1, upper(Sw2), Sw2, tolower(Sw2), Sw3, tolower(Sw3))
-  if(is.null(grouping.var)){
-    dat <- as.data.frame(rep("all", length(text.var)), 
-                         drop = FALSE, stringsAsFactors = FALSE)
-    names(dat) <- "all"
-    grouping.var <- dat
-  }
-  group.var <- if (is.list(grouping.var) & length(grouping.var)>1) {
-    apply(data.frame(grouping.var), 1, function(x){
-      if (any(is.na(x))){
-        NA
-      } else {
-        paste(x, collapse = ".")
-      }
-    }
-    )
-  } else {
-    grouping.var
-  }   
-  NAME <- if (is.list(grouping.var)) {
-    m <- unlist(as.character(substitute(grouping.var))[-1])
-    m <- sapply(strsplit(m, "$", fixed=TRUE), 
-                function(x) x[length(x)], USE.NAMES = FALSE)
-    paste(m, collapse="&")
-  } else {
-    G <- as.character(substitute(grouping.var))
-    G[length(G)]
-  }
-  word.lists1 <- textLISTER(text.var = text.var, group.vars = group.var,
-                   rm.bracket = rm.bracket)
-  words.UNLISTED <- lapply(word.lists1, function(x) {
-    y <- unlist(x)
-    names(y) <- NULL
-    return(y)
-  }
-  ) 
-  if (cap){ 
-    word.lists2 <- lapply(word.lists1, function(x) {
-      y <- capitalizer(x, caps.list=cap.list)
-      names(y) <- NULL
-      return(y)
-    }
-    )    
-  } else {
-    word.lists2 <- lapply(word.lists1, function(x) {
-      y <- unlist(x)
-      names(y) <- NULL
-      return(y)
-    }
-    ) 
-  }
-  naomit <- function(x) x[!is.na(x)]
-  word.lists2 <- lapply(word.lists2, naomit)
-  stopped.word.list <- lapply(words.UNLISTED, function(x) {
-    x[!x %in% stopwords]
-  }
-  )
-  stopped.word.list <- lapply(stopped.word.list, naomit)
-  stopped.word.list <- lapply(stopped.word.list, function(x){ 
-    capitalizer(x, caps.list = cap.list)
-  }
-  )
-  COUNT <- function(x) {
-    if (is.null(x) | identical(x, character(0)) | 
-      identical(x, logical(0))) {
-      DF <- data.frame(WORD=NA, FREQ=NA)
+    cut.n = 20, cap = TRUE, cap.list=NULL, cap.I=TRUE, rm.bracket = TRUE) {
+    upper <- function(x) paste(substring(x, 1, 1), 
+        substring(x, 2, nchar(x)), sep="")
+    Sw1 <- stopwords[!substring(stopwords, 1, 1) %in% LETTERS]
+    Sw2 <- stopwords[substring(stopwords, 1, 1) %in% LETTERS]
+    Sw3 <- if (!is.null(Sw2)) {
+        paste0(Sw2, "'s")
     } else {
-      DF <- data.frame(table(x))
-      names(DF) <- c("WORD", "FREQ")
-      DF$WORD <- as.character(DF$WORD)
-      DF$FREQ <- as.numeric(DF$FREQ)
-      DF <- DF[rev(order(DF$FREQ)), ]
-      rownames(DF) <- NULL
+        NULL
     }
-    return(DF)
-  }
-  freq.word.list <- lapply(word.lists2, COUNT)
-  freq.stop.word.list <- lapply(stopped.word.list, COUNT)
-  red.freq.stop.word.list <- ncutWORDS(freq.stop.word.list, cut.n = cut.n) 
-  word.lists2 <- lapply(word.lists2, function(x) {
-    if (is.null(x)){
-      return(x)
-    } else { 
-      comment(x) <- "bagOwords"
-      return(x)
+    stopwords <- c(Sw1, upper(Sw2), Sw2, tolower(Sw2), Sw3, tolower(Sw3))
+    if(is.null(grouping.var)) {
+        dat <- as.data.frame(rep("all", length(text.var)), 
+            drop = FALSE, stringsAsFactors = FALSE)
+        names(dat) <- "all"
+        grouping.var <- dat
     }
-  }
-  )
-  stopped.word.list <- lapply(stopped.word.list, function(x) {
-    if (is.null(x)){
-      return(x)
-    } else { 
-      comment(x) <- "bagOwords"
-      return(x)
+    group.var <- if (is.list(grouping.var) & length(grouping.var)>1) {
+        apply(data.frame(grouping.var), 1, function(x){
+                if (any(is.na(x))){
+                    NA
+                } else {
+                    paste(x, collapse = ".")
+                }
+            }
+        )
+    } else {
+        grouping.var
+    }   
+    NAME <- if (is.list(grouping.var)) {
+        m <- unlist(as.character(substitute(grouping.var))[-1])
+        m <- sapply(strsplit(m, "$", fixed=TRUE), 
+            function(x) x[length(x)], USE.NAMES = FALSE)
+            paste(m, collapse="&")
+    } else {
+        G <- as.character(substitute(grouping.var))
+        G[length(G)]
     }
-  }
-  )
-  freq.word.list <- lapply(freq.word.list, function(x) {
-    if (is.null(x)){
-      return(x)
-    } else { 
-      comment(x) <- "freqList"
-      return(x)
+    word.lists1 <- textLISTER(text.var = text.var, group.vars = group.var,
+        rm.bracket = rm.bracket)
+    words.UNLISTED <- lapply(word.lists1, function(x) {
+            y <- unlist(x)
+            names(y) <- NULL
+            return(y)
+        }
+    ) 
+    if (cap) { 
+        word.lists2 <- lapply(word.lists1, function(x) {
+                y <- capitalizer(x, caps.list=cap.list)
+                names(y) <- NULL
+                return(y)
+            }
+        )    
+    } else {
+        word.lists2 <- lapply(word.lists1, function(x) {
+                y <- unlist(x)
+                names(y) <- NULL
+                return(y)
+            }
+        ) 
     }
-  }
-  )
-  freq.stop.word.list <- lapply(freq.stop.word.list, function(x) {
-    if (is.null(x)){
-      return(x)
-    } else { 
-      comment(x) <- "freqList"
-      return(x)
+    naomit <- function(x) x[!is.na(x)]
+    word.lists2 <- lapply(word.lists2, naomit)
+    stopped.word.list <- lapply(words.UNLISTED, function(x) {
+            x[!x %in% stopwords]
+        }
+    )
+    stopped.word.list <- lapply(stopped.word.list, naomit)
+    stopped.word.list <- lapply(stopped.word.list, function(x){ 
+            capitalizer(x, caps.list = cap.list)
+        }
+    )
+    COUNT <- function(x) {
+        if (is.null(x) | identical(x, character(0)) | 
+            identical(x, logical(0))) {
+            DF <- data.frame(WORD=NA, FREQ=NA)
+        } else {
+            DF <- data.frame(table(x))
+            names(DF) <- c("WORD", "FREQ")
+            DF$WORD <- as.character(DF$WORD)
+            DF$FREQ <- as.numeric(DF$FREQ)
+            DF <- DF[order(-DF$FREQ, DF$WORD), ]
+            rownames(DF) <- NULL
+        }
+        return(DF)
     }
-  }
-  )
-  red.freq.stop.word.list <- lapply(red.freq.stop.word.list, function(x) {
-    if (is.null(x)){
-      return(x)
-    } else { 
-      comment(x) <- "freqList"
-      return(x)
-    }
-  }
-  )
-  comment(word.lists2) <- "cwl"    
-  comment(stopped.word.list) <- "swl"
-  comment(freq.word.list) <- "fwl" 
-  comment(freq.stop.word.list) <- "fswl"
-  comment(red.freq.stop.word.list) <- "rfswl"
-  if (alphabetical) {
-    asort <- function(dat, col=1) {
-      dat2 <-dat[order(dat[, col]), ]
-      rownames(dat2) <- NULL
-      return(dat2)
-    }
-    freq.word.list <- lapply(freq.word.list, asort)
-    freq.stop.word.list <- lapply(freq.stop.word.list, asort)
-    red.freq.stop.word.list <- lapply(red.freq.stop.word.list, asort)
-  }  
-  o <- list(cwl = word.lists2, swl = stopped.word.list, 
-            fwl = freq.word.list, fswl = freq.stop.word.list, 
-            rfswl = red.freq.stop.word.list)
-  class(o) <- "word.list"
-  return(o)
+    freq.word.list <- lapply(word.lists2, COUNT)
+    freq.stop.word.list <- lapply(stopped.word.list, COUNT)
+    red.freq.stop.word.list <- ncutWORDS(freq.stop.word.list, cut.n = cut.n) 
+    word.lists2 <- lapply(word.lists2, function(x) {
+            if (is.null(x)){
+                return(x)
+            } else { 
+                comment(x) <- "bagOwords"
+                return(x)
+            }
+        }
+    )
+    stopped.word.list <- lapply(stopped.word.list, function(x) {
+        if (is.null(x)){
+                return(x)
+        } else { 
+                comment(x) <- "bagOwords"
+                return(x)
+            }
+        }
+    )
+    freq.word.list <- lapply(freq.word.list, function(x) {
+            if (is.null(x)) {
+                return(x)
+            } else { 
+                comment(x) <- "freqList"
+                return(x)
+            }
+        }
+    )
+    freq.stop.word.list <- lapply(freq.stop.word.list, function(x) {
+            if (is.null(x)) {
+                return(x)
+            } else { 
+                comment(x) <- "freqList"
+                return(x)
+            }
+        }
+    )
+    red.freq.stop.word.list <- lapply(red.freq.stop.word.list, function(x) {
+            if (is.null(x)){
+                return(x)
+            } else { 
+                comment(x) <- "freqList"
+                return(x)
+            }
+        }
+    )
+    comment(word.lists2) <- "cwl"    
+    comment(stopped.word.list) <- "swl"
+    comment(freq.word.list) <- "fwl" 
+    comment(freq.stop.word.list) <- "fswl"
+    comment(red.freq.stop.word.list) <- "rfswl"
+    if (alphabetical) {
+        asort <- function(dat, col=1) {
+            dat2 <-dat[order(dat[, col]), ]
+            rownames(dat2) <- NULL
+            return(dat2)
+        }
+        freq.word.list <- lapply(freq.word.list, asort)
+        freq.stop.word.list <- lapply(freq.stop.word.list, asort)
+        red.freq.stop.word.list <- lapply(red.freq.stop.word.list, asort)
+    } 
+    o <- list(cwl = word.lists2, swl = stopped.word.list, 
+        fwl = freq.word.list, fswl = freq.stop.word.list, 
+        rfswl = red.freq.stop.word.list)
+    class(o) <- "word.list"
+    return(o)
 }
 
 qda <- word_list
