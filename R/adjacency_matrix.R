1a30a0c07aea3cf6bab6bf18c3e55732ae4b9b79
tyler rinker <tyler_rinker@hotmail.com>
2012-08-12 00:43:18 -0400

    added documentation for adjmat

--- R/adjacency_matrix.R
+++ R/adjacency_matrix.R
@@ -1,66 +1,18 @@
 #' Takes a matrix and generates an adjacency matrix
 #' 
-#' Takes a matrix (wfm) or termco object (.c or .d) and generates an adjacency
+#' Takes a matrix (wfm) or termco object (.a, .c or .d) and generates an adjacency
 #' matrix for use with igraph
 #' 
-#' %% ~~ If necessary, more details than the description above ~~
-#' 
 #' @aliases adjacency_matrix adjmat
-#' @param matrix.obj %% ~~Describe \code{matrix.obj} here~~
-#' @return %% ~Describe the value returned %% If it is a LIST, use %%
-#' \item{comp1 }{Description of 'comp1'} %% \item{comp2 }{Description of
-#' 'comp2'} %% ...
-#' @note %% ~~further notes~~
-#' @author %% ~~who you are~~
-#' @seealso %% ~~objects to See Also as \code{\link{help}}, ~~~
-#' @references %% ~put references to the literature/web site here ~
-#' @keywords ~kwd1 ~kwd2
+#' @param matrix.obj A amtrix object, preferably, of the class "termco_d" or "termco_c" generated from terco.a, termco.d or termco.c.
+#' @return Generates an adjacency matrix
+#' @seealso 
+#' \code{\link[stats]{dist}}
+#' @keywords adjacency matrix
 #' @examples
-#' 
-#' ##---- Should be DIRECTLY executable !! ----
-#' ##-- ==>  Define data, use random,
-#' ##--	or do  help(data=index)  for the standard data sets.
-#' 
-#' ## The function is currently defined as
-#' function (matrix.obj) 
-#' {
-#'     if (class(matrix.obj) %in% c("termco_d", "termco_c")) {
-#'         info <- matrix.obj
-#'         if (matrix.obj[["zero_replace"]] != 0) {
-#'             matrix.obj <- replacer(matrix.obj[["raw"]], matrix.obj[["zero_replace"]], 
-#'                 0)
-#'         }
-#'         else {
-#'             matrix.obj <- matrix.obj[["raw"]]
-#'         }
-#'         matrix.obj <- termco2mat(matrix.obj)
-#'     }
-#'     else {
-#'         if (class(matrix.obj) == "matrix") {
-#'             if (is.null(comment(matrix.obj))) {
-#'                 warning("Not a termco or wfm object; results may not be correct.")
-#'             }
-#'             else {
-#'                 if (comment(matrix.obj) != "true.matrix") {
-#'                   warning(paste("Not a termco.d, termco.c or wfm object;", 
-#'                     "results may not be correct."))
-#'                 }
-#'             }
-#'         }
-#'         else {
-#'             warning("Not a matrix object; results may not be correct.")
-#'         }
-#'     }
-#'     Y <- matrix.obj >= 1
-#'     Y <- apply(Y, 2, as, "numeric")
-#'     rownames(Y) <- rownames(matrix.obj)
-#'     Z <- Z2 <- t(Y) %*% Y
-#'     Z2[!lower.tri(Z2)] <- NA
-#'     Z2 <- Z2[-1, -ncol(Z2)]
-#'     o <- list(boolean = Y, adjacency = Z, shared = Z2, sums = colSums(Y))
-#'     class(o) <- "adjacency.matrix"
-#'     return(o)
-#'   }
+#' wordLIST <- c(" montague", " capulet", " court", " marry")
+#' (raj.termco <- with(raj.act.1, termco.a(dialogue, person, wordLIST, ignore.case = T)))
+#' (raj.adjmat <- adjmat(raj.termco))
 #' 
 adjacency_matrix <-
 function(matrix.obj) {
