ef1a0d1c58cb4d03f69c306a8b7fd17e283c6512
tyler rinker <tyler_rinker@hotmail.com>
2012-08-13 16:09:50 -0400

    added rajSPLIT data set which is a sent split verion of romeo and juliet

--- R/automated_readability_index.R
+++ R/automated_readability_index.R
@@ -1,80 +1,25 @@
+#' Automated Readability Index
+#' 
 #' Transcript Apply Automated Readability Index
 #' 
-#' %% ~~ A concise (1-5 lines) description of what the function does. ~~
-#' 
-#' %% ~~ If necessary, more details than the description above ~~
-#' 
-#' @param text.var %% ~~Describe \code{text.var} here~~
-#' @param grouping.var %% ~~Describe \code{grouping.var} here~~
-#' @return %% ~Describe the value returned %% If it is a LIST, use %%
-#' \item{comp1 }{Description of 'comp1'} %% \item{comp2 }{Description of
-#' 'comp2'} %% ...
-#' @note %% ~~further notes~~
-#' @author %% ~~who you are~~
-#' @seealso %% ~~objects to See Also as \code{\link{help}}, ~~~
-#' @references %% ~put references to the literature/web site here ~
-#' @keywords ~kwd1 ~kwd2
+#' @param text.var The text variable
+#' @param group.vars The grouping variables.  Default NULL generates one word list for all text.  Also takes a single grouping variable or a list of 1 or more grouping variables.
+#' @return Generates the Automated Readability Index by grouping variable(s)
+#' @note The Automated Readability Index is a readability score which approximates the grade level needed to comprehend the text. The Automated Readability Index is derived from word difficulty (letters per word) and sentence difficulty (words per sentence).
+#' @warning If you have not run the sentSplit function on your data the results will not be accurate.
+#' @seealso
+#' \code{\link[qdap]{SMOG}}, 
+#' \code{\link[qdap]{flesch_kincaidg}},
+#' \code{\link[qdap]{frey}},
+#' \code{\link[qdap]{coleman_liau}},
+#' \code{\link[qdap]{linsear_write}},
+#' \code{\link[qdap]{word_stats}}
+#' @references R. J. Senter and E. A. Smith. (1967) Automated readability index. Technical Report AMRLTR-66-220, University of Cincinnati, Cincinnati, Ohio.
+#' @keywords readability, Automated Readability Index
 #' @examples
-#' 
-#' ##---- Should be DIRECTLY executable !! ----
-#' ##-- ==>  Define data, use random,
-#' ##--	or do  help(data=index)  for the standard data sets.
-#' 
-#' ## The function is currently defined as
-#' function (text.var, grouping.var = NULL) 
-#' {
-#'     G <- if (is.null(grouping.var)) {
-#'         "all"
-#'     }
-#'     else {
-#'         if (is.list(grouping.var)) {
-#'             m <- unlist(as.character(substitute(grouping.var))[-1])
-#'             m <- sapply(strsplit(m, "$", fixed = TRUE), function(x) x[length(x)])
-#'             paste(m, collapse = "&")
-#'         }
-#'         else {
-#'             G <- as.character(substitute(grouping.var))
-#'             G[length(G)]
-#'         }
-#'     }
-#'     grouping <- if (is.null(grouping.var)) {
-#'         rep("all", length(text.var))
-#'     }
-#'     else {
-#'         if (is.list(grouping.var) & length(grouping.var) > 1) {
-#'             apply(data.frame(grouping.var), 1, function(x) {
-#'                 if (any(is.na(x))) {
-#'                   NA
-#'                 }
-#'                 else {
-#'                   paste(x, collapse = ".")
-#'                 }
-#'             })
-#'         }
-#'         else {
-#'             unlist(grouping.var)
-#'         }
-#'     }
-#'     text <- as.character(text.var)
-#'     DF <- na.omit(data.frame(group = grouping, text.var = text, 
-#'         stringsAsFactors = FALSE))
-#'     DF$word.count <- word.count(DF$text.var, missing = 0)
-#'     i <- as.data.frame(table(DF$group))
-#'     DF$group <- DF$group[, drop = TRUE]
-#'     DF$tot.n.sent <- 1:nrow(DF)
-#'     DF <- DF[with(DF, order(group, DF$tot.n.sent)), ]
-#'     DF$character.count <- character.count(DF$text.var)
-#'     DF2 <- aggregate(word.count ~ group, DF, sum)
-#'     DF2$sentence.count <- as.data.frame(table(DF$group))$Freq
-#'     DF2$character.count <- aggregate(character.count ~ group, 
-#'         DF, sum)$character.count
-#'     ari <- function(tse, tw, tc) 4.71 * (tc/tw) + 0.5 * (tw/tse) - 
-#'         21.43
-#'     DF2$Automated_Readability_Index <- round(with(DF2, ari(tse = sentence.count, 
-#'         tc = character.count, tw = word.count)), digits = 1)
-#'     names(DF2) <- c(G, names(DF2)[-1])
-#'     return(DF2)
-#'   }
+#' rajDEM <- key_merge(raj, raj.demographics, 'person')
+#' with(rajDEM, automated_readability_index(dialogue, list(person, act)))
+#' with(rajDEM, automated_readability_index(dialogue, list(sex, fam.aff)))
 #' 
 automated_readability_index <-
 function(text.var, grouping.var = NULL) {
