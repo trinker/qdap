ab44200f5c082e01ff6f856c1ae08ef192df4776
tyler rinker <tyler_rinker@hotmail.com>
2012-08-12 23:59:47 -0400

    added a insertDummy function with roxygen documentation

---
+++ R/insertDummy.R
@@ -0,0 +1,56 @@
+#' Range Coding of a splitWord Matrix
+#' 
+#' Allows range coding of words for efficient coding.
+#' 
+#' @param dataframe A dataframe containing a text variable.
+#' @param ranges A named list of ranges to recode.  Namses correspond to code names in dataframe.
+#' @param value The recode value.  Takes a vector of length one or a vector of length equal to the number of code columns.
+#' @param text.var The name of the text variable.
+#' @param codes Optional vector of codes.
+#' @param transform logical.  If TRUE the words are located across the top of dataframe.
+#' @return Generates a dummy coded dataframe.
+#' @note %% ~~further notes~~
+#' @seealso 
+#' \code{\link{wordSplit}}
+#' \code{\link{code2long}}
+#' \code{\link{transform.code.matrix}}
+#' @references Miles, M. B. & Huberman, A. M. (1994). An expanded sourcebook: Qualitative   data analysis. 2nd ed. Thousand Oaks, CA: SAGE Publications.
+#' @keywords coding
+#' @examples
+#' codes <- qcv(dc, sf, wes, pol, rejk, lk, azx, mmm)
+#' X <- wordSplit(DATA, "state", codes)
+#' insertDummy(X, list(dc=c(1:3, 5),  sf=c(4, 6:9, 11), wes=0, pol=0, rejk=0, lk=0, azx=1:30, mmm=5))
+#'
+insertDummy <-
+function(dataframe, ranges, value = 1, text.var = NULL, code.vars = NULL, 
+    transform = FALSE) {
+    if (transform) {
+        dataframe <- transform.code.matrix(dataframe = dataframe, 
+            text.var = text.var, code.vars = code.vars)
+    }
+    if (!is.null(text.var) && !is.numeric(text.var)) {
+        text.var <- which(colnames(DF) == text.var)
+    }
+    if (!is.null(code.vars) && !is.numeric(code.vars)) {
+        code.vars <- which(colnames(DF) %in% c(code.vars))
+    }
+    if (is.null(text.var)) {
+        text.var <- which(colnames(dataframe) == "text")
+    }
+    if (is.null(code.vars)) {
+        code.vars <- (text.var + 2):ncol(dataframe)
+    }
+    left.overs <- which(!1:ncol(dataframe) %in% c(code.vars, text.var))
+    dataframe[, text.var] <- as.character(dataframe[, text.var])
+    if (length(value) == 1) {
+        value <- rep(value, length(code.vars))
+    }
+    CV <- dataframe[, code.vars]
+    lapply(1:ncol(CV), function(i) {
+            CV[ ranges[[colnames(CV)[i]]], colnames(CV)[i]] <<- value[i]
+            return(CV)
+        }
+    )
+    DF <- data.frame(dataframe[, 1:(text.var + 1)], CV, stringsAsFactors = FALSE)
+    return(DF)
+}
--- R/wordSplit.R
+++ R/wordSplit.R
@@ -4,14 +4,14 @@
 #' 
 #' @param dataframe A dataframe containing a text variable.
 #' @param text.var The name of the text variable.
-#' @param codes Optional list of codes .
+#' @param codes Optional list of codes.
 #' @param csv logical.  If TRUE creates a csv in the working directory.
 #' @param file.name The name of the csv file.  If NULL defaults to the dtaframe name.
 #' @param transpose logical.  If TRUE transposes the dataframe so that the text is across the top. 
 #' @param strip logical.  If TRUE all punctuation is removed.
 #' @return Generates a dataframe, and optional csv file, of individual words while maintaing demgraphic information.  If a vector of codes is provided the outcome is a matrix of words used by codes filled with zeros.  This dataframe is useful for dummy coded (1-yes code exists; 2-no it does not) representation of data and can be used for visualizations and statistical analysis.
-#' @note %% ~~further notes~~
 #' @seealso 
+#' \code{\link{insertDummy}}
 #' \code{\link{code2long}}
 #' \code{\link{transform.code.matrix}}
 #' @references Miles, M. B. & Huberman, A. M. (1994). An expanded sourcebook: Qualitative   data analysis. 2nd ed. Thousand Oaks, CA: SAGE Publications.
