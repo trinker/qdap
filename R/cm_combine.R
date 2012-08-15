951efb0150343e95664aae280a8d35401c083b76
tyler rinker <tyler_rinker@hotmail.com>
2012-08-14 23:51:27 -0400

    added sm_combine to the line up

---
+++ R/cm_combine.R
@@ -0,0 +1,17 @@
+cm_combine <-
+function(dataframe, combined.columns, elim.old = FALSE) { 
+    combo <- function(dat, comb, new.name, elim.old) {
+        DF <- data.frame(dat, as.numeric(rowSums(dat[, comb]) > 0))
+        names(DF)[ncol(DF)] <- new.name
+        if (elim.old) {
+            DF <- DF[, !names(DF) %in% comb]
+        }
+        return(DF)
+    }
+    lapply(seq_along(combined.columns), function(i) {
+        dataframe <<- combo(dat = dataframe, comb = combined.columns[[i]], 
+            new.name = names(combined.columns)[i], elim.old = elim.old)
+        }
+    )
+    return(dataframe)
+}
--- R/termco.c.R
+++ R/termco.c.R
@@ -19,34 +19,6 @@
 #' @references %% ~put references to the literature/web site here ~
 #' @keywords ~kwd1 ~kwd2
 #' @examples
-
-
-
-
-
-
-
-#' Combine Columns from a termco Object
-#' 
-#' %% ~~ A concise (1-5 lines) description of what the function does. ~~
-#' 
-#' %% ~~ If necessary, more details than the description above ~~
-#' 
-#' @aliases termco.c print.termco_c
-#' @param termco.d.object %% ~~Describe \code{termco.d.object} here~~
-#' @param combined.columns %% ~~Describe \code{combined.columns} here~~
-#' @param new.name %% ~~Describe \code{new.name} here~~
-#' @param zero.replace %% ~~Describe \code{zero.replace} here~~
-#' @param elim.old %% ~~Describe \code{elim.old} here~~
-#' @return %% ~Describe the value returned %% If it is a LIST, use %%
-#' \item{comp1 }{Description of 'comp1'} %% \item{comp2 }{Description of
-#' 'comp2'} %% ...
-#' @note %% ~~further notes~~
-#' @author %% ~~who you are~~
-#' @seealso %% ~~objects to See Also as \code{\link{help}}, ~~~
-#' @references %% ~put references to the literature/web site here ~
-#' @keywords ~kwd1 ~kwd2
-#' @examples
 #' 
 #' ##---- Should be DIRECTLY executable !! ----
 #' ##-- ==>  Define data, use random,
