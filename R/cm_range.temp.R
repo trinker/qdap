dc5f7fd1bd894d204f1b224e97af2940efba7c6e
tyler rinker <tyler_rinker@hotmail.com>
2012-08-28 01:34:42 -0400

    added a write 2 clipboard ability for mac and windows

--- R/cm_range.temp.R
+++ R/cm_range.temp.R
@@ -1,7 +1,19 @@
-cm_range.temp <-
+cm_range.temp <- 
 function(codes) {
     cat(paste("list(\n",
         paste0("    ", paste0("\b", paste(codes, 
             collapse = " = c(),\n    "), " = c()")), "\n)\n")
     )
+    x <- matrix(c("list(", 
+        paste0("    ", codes[1:(length(codes)-1)], " = c(),"),
+        paste0("    ", codes[length(codes)], " = c()"),
+        ")"), ncol = 1)
+    if (Sys.info()["sysname"] == "Windows") {                
+        writeClipboard(x, format = 1)                        
+    }                                                        
+    if (Sys.info()["sysname"] == "Darwin") {                 
+        j <- pipe("pbcopy", "w")                             
+        writeLines(x, con = j)                               
+        close(j)                                             
+    }                   
 }
