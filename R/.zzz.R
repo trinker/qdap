b6ed393f9584d2f9f1e01b42fc1a03549daf1cd6
tyler rinker <tyler_rinker@hotmail.com>
2012-08-30 10:23:31 -0400

    added pacman functions to lead XML and RCurl

---
+++ R/.zzz.R
@@ -0,0 +1,19 @@
+.onLoad <- function(libname = find.package("qdap"), 
+    pkgname = "qdap") {
+    if (!"pacman" %in% .packages(all.available = TRUE)) {
+        install.packages("devtools")
+        library(devtools)
+        install_github("pacman", "trinker")
+    }
+    if (!"openNLP" %in% .packages(all.available = TRUE)) {
+        install.packages("openNLP", type = "source")
+    }    
+    require(pacman)
+    if (!"XML" %in% .packages(all.available = TRUE)) {
+        p_getRipley("XML")
+    }
+    if (!"XML" %in% .packages(all.available = TRUE)) {
+        p_getRipley("RCurl")
+    }    
+    p_load(openNLP, XML, RCurl)
+}
