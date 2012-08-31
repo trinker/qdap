353cb56d29d4f58a86c9574a7d9ab93debdbeb3b
tyler rinker <tyler_rinker@hotmail.com>
2012-08-30 20:40:44 -0400

    hold the R.zzz

--- R/.zzz.R
+++ R/.zzz.R
@@ -1,21 +1,23 @@
-.onLoad <- function(libname = find.package("qdap"), 
-    pkgname = "qdap") {
-    if (!"pacman" %in% .packages(all.available = TRUE)) {
-        install.packages("devtools")
-        library(devtools)
-        install_github("pacman", "trinker")
-    }
-    if (!"openNLP" %in% .packages(all.available = TRUE)) {
-        install.packages("openNLP", type = "source")
-    }    
-    require(pacman)
-    if (!"XML" %in% .packages(all.available = TRUE)) {
-        p_getRipley("XML")
-    }
-    if (!"XML" %in% .packages(all.available = TRUE)) {
-        p_getRipley("RCurl")
-    }    
-    require(openNLP)
-    require(XML)
-    require(RCurl)
-}
+##############################################################
+# .onLoad <- function(libname = find.package("qdap"),        #
+#     pkgname = "qdap") {                                    #
+#     if (!"pacman" %in% .packages(all.available = TRUE)) {  #
+#         install.packages("devtools")                       #
+#         library(devtools)                                  #
+#         install_github("pacman", "trinker")                #
+#     }                                                      #
+#     if (!"openNLP" %in% .packages(all.available = TRUE)) { #
+#         install.packages("openNLP", type = "source")       #
+#     }                                                      #
+#     require(pacman)                                        #
+#     if (!"XML" %in% .packages(all.available = TRUE)) {     #
+#         p_getRipley("XML")                                 #
+#     }                                                      #
+#     if (!"XML" %in% .packages(all.available = TRUE)) {     #
+#         p_getRipley("RCurl")                               #
+#     }                                                      #
+#     require(openNLP)                                       #
+#     require(XML)                                           #
+#     require(RCurl)                                         #
+# }                                                          #
+##############################################################
