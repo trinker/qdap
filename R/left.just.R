e9ba1e4abdcae878f16247392247485e2fc8e73b
tyler rinker <tyler_rinker@hotmail.com>
2012-08-31 17:50:12 -0400

    headers are left justified now too to solve issue #60

--- R/left.just.R
+++ R/left.just.R
@@ -16,75 +16,26 @@
 #' @keywords ~kwd1 ~kwd2
 #' @examples
 #' 
-#' ##---- Should be DIRECTLY executable !! ----
-#' ##-- ==>  Define data, use random,
-#' ##--	or do  help(data=index)  for the standard data sets.
-#' 
-#' ## The function is currently defined as
-#' function (dataframe, column = NULL) 
-#' {
-#'     left.j <- function(x) {
-#'         n <- max(nchar(x))
-#'         return(sprintf(paste("%-", n, "s", sep = ""), x))
-#'     }
-#'     lj <- function(dataframe, column) {
-#'         DF2 <- dataframe
-#'         if (is.null(column)) 
-#'             column <- names(dataframe)
-#'         Q <- max(nchar(as.character(DF2[, column])))
-#'         DF2[, column] <- left.j(as.character(DF2[, column]))
-#'         if (is.character(column)) {
-#'             col <- names(DF2)[which(names(DF2) == column)]
-#'             names(DF2)[which(names(DF2) == column)] <- sprintf(paste("%-", 
-#'                 Q, "s", sep = ""), col)
-#'         }
-#'         else {
-#'             if (is.numeric(column)) {
-#'                 col <- names(DF2)[column]
-#'                 names(DF2)[column] <- sprintf(paste("%-", Q, 
-#'                   "s", sep = ""), col)
-#'             }
-#'         }
-#'         return(DF2)
-#'     }
-#'     if (length(column) < 2) {
-#'         if (!is.data.frame(dataframe)) {
-#'             y <- as.character(substitute(dataframe))
-#'             dataframe <- data.frame(dataframe)
-#'             y <- if (y[1] %in% c("[", "$")) 
-#'                 y[2]
-#'             else y[1]
-#'             names(dataframe) <- y
-#'         }
-#'         DF3 <- lj(dataframe = dataframe, column = column)
-#'     }
-#'     else {
-#'         if (!is.numeric(column)) 
-#'             column <- match(column, names(dataframe))
-#'         dat <- dataframe[, -c(column)]
-#'         ndf <- colnames(dataframe)
-#'         LIST <- lapply(column, function(x) lj(dataframe = dataframe[, 
-#'             x, drop = FALSE], column = NULL))
-#'         dat2 <- cbind(do.call("cbind", LIST), dat, checknames = FALSE)
-#'         NAMES <- colnames(dat2)
-#'         newloc <- match(ndf, Trim(NAMES))
-#'         DF3 <- dat2[, newloc]
-#'     }
-#'     return(DF3)
-#'   }
-#' 
 left.just <-
 function(dataframe, column = NULL) {
+    df.class <- function(dataframe) {
+        sapply(1:ncol(dataframe), function(i) {
+            x <- class(dataframe[, i])
+            x[length(x)]
+        })
+    }
+    CLASS <- df.class(dataframe)
     left.j <- function(x) {
         n <- max(nchar(x))
         return(sprintf(paste("%-", n, "s", sep = ""), x))
     }
-    lj <- function(dataframe, column) {
-        DF2 <- dataframe
-        if (is.null(column)) column <- names(dataframe)
-        Q <- max(nchar(as.character(DF2[, column])))
+    if (is.null(column)) column <- colnames(dataframe)
+    lj <- function(DF2, column) {
+        if (is.null(column)) column <- colnames(DF2)
+        Q <- max(nchar(c(as.character(DF2[, column]), names(DF2)[column])))
+        DF2 <- data.frame(rbind(colnames(DF2), do.call(cbind,
+            lapply(DF2, as.character))))
         DF2[, column] <- left.j(as.character(DF2[, column]))     
-
         if (is.character(column)) {
             col <- names(DF2)[which(names(DF2) == column)]
                 names(DF2)[which(names(DF2) == column)] <- sprintf(paste("%-", 
@@ -96,7 +47,9 @@ function(dataframe, column = NULL) {
                     sep = ""), col)
             }
         }
-    return(DF2)
+        DF2 <- data.frame(DF2[-1, , drop = FALSE], check.names = FALSE)
+        rownames(DF2) <- NULL
+        return(DF2)
     }
     if (length(column)<2) {
         if (!is.data.frame(dataframe)) {
@@ -105,18 +58,32 @@ function(dataframe, column = NULL) {
             y <- if (y[1]%in%c("[", "$")) y[2] else y[1]
             names(dataframe) <- y
         }
-        DF3 <- lj(dataframe=dataframe, column=column)
+        DF3 <- lj(DF2=dataframe, column=column)
     } else { 
         if (!is.numeric(column)) column <- match(column, names(dataframe))
         dat <- dataframe[, -c(column)]
         ndf <- colnames(dataframe)
-        LIST <- lapply(column, function(x) lj(dataframe=
-            dataframe[, x, drop=FALSE], column = NULL))
-        dat2 <- cbind(do.call('cbind', LIST), dat, checknames=FALSE)
+        LIST <- lapply(column, function(x) {
+            lj(DF2=dataframe[, x, drop=FALSE], column = NULL)
+        })
+        dat2 <- data.frame(cbind(do.call('cbind', LIST), dat), checknames=FALSE)
         NAMES <- colnames(dat2)
-        newloc <- match(ndf, Trim(NAMES))
-        
+        STrim <- function (x) gsub("^\\s+|\\s+$|\\.+$", "", x)
+        newloc <- match(ndf, STrim(NAMES))
         DF3 <- dat2[, newloc]
     }
+    colClasses <- function(d, colClasses) {
+        colClasses <- rep(colClasses, len=length(d))
+        d[] <- lapply(seq_along(d), function(i) switch(colClasses[i], 
+            numeric=as.numeric(d[[i]]), 
+            character=as.character(d[[i]]), 
+            Date=as.Date(d[[i]], origin='1970-01-01'), 
+            POSIXct=as.POSIXct(d[[i]], origin='1970-01-01'), 
+            factor=as.factor(d[[i]]),
+            as(d[[i]], colClasses[i]) ))
+        d
+    }
+    DF3 <- colClasses(DF3, CLASS)
+    colnames(DF3) <- gsub("\\.(?=\\.*$)", " ", colnames(DF3), perl=TRUE)
     return(DF3)
 }
