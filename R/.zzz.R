903b05637f845752036594655a0b9ab9d157baf9
tyler rinker <tyler_rinker@hotmail.com>
2012-08-30 22:16:08 -0400

    attempt at .on.load

--- R/.zzz.R
+++ R/.zzz.R
@@ -1,23 +1,24 @@
-##############################################################
-# .onLoad <- function(libname = find.package("qdap"),        #
-#     pkgname = "qdap") {                                    #
-#     if (!"pacman" %in% .packages(all.available = TRUE)) {  #
-#         install.packages("devtools")                       #
-#         library(devtools)                                  #
-#         install_github("pacman", "trinker")                #
-#     }                                                      #
-#     if (!"openNLP" %in% .packages(all.available = TRUE)) { #
-#         install.packages("openNLP", type = "source")       #
-#     }                                                      #
-#     require(pacman)                                        #
-#     if (!"XML" %in% .packages(all.available = TRUE)) {     #
-#         p_getRipley("XML")                                 #
-#     }                                                      #
-#     if (!"XML" %in% .packages(all.available = TRUE)) {     #
-#         p_getRipley("RCurl")                               #
-#     }                                                      #
-#     require(openNLP)                                       #
-#     require(XML)                                           #
-#     require(RCurl)                                         #
-# }                                                          #
-##############################################################
+.onLoad <- function(libname = find.package("qdap"),        
+    pkgname = "qdap") {                                    
+#     if (!"pacman" %in% .packages(all.available = TRUE)) {  
+#         install.packages("devtools")                       
+#         library(devtools)                                  
+#         install_github("pacman", "trinker")                
+#     }                                                      
+    if (!"openNLP" %in% .packages(all.available = TRUE)) { 
+        install.packages("openNLP", type = "source")       
+    }                                                      
+    require(pacman)                                        
+    if (!"XML" %in% .packages(all.available = TRUE)) {     
+        URL <- "http://www.stats.ox.ac.uk/pub/RWin/bin/windows/contrib/2.15/"
+        install.packages("RCurl", contriburl = URL)                                
+    }                                                      
+    if (!"XML" %in% .packages(all.available = TRUE)) {     
+        URL <- "http://www.stats.ox.ac.uk/pub/RWin/bin/windows/contrib/2.15/"
+        install.packages("XML", contriburl = URL)                       
+    }                                                      
+    require(openNLP)                                       
+    require(XML)                                           
+    require(RCurl)                                         
+} 
+#, openNLP, XML, RCurl
