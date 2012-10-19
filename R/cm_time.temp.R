a97be9136c29814903def1a93cc658c11579b072
tyler rinker <tyler_rinker@hotmail.com>
2012-10-19 11:29:47 -0400

    added qcv

--- R/cm_time.temp.R
+++ R/cm_time.temp.R
@@ -44,7 +44,7 @@ function(codes, start = ":00", end, file=NULL) {
     cat(paste0("list(\n",
         "    transcript_time_span = qcv(00:00 - 00:00),\n",
         paste0("    ", paste0(paste(codes, 
-        collapse = " = c(),\n    "), " = c()")), "\n)\n"))
+        collapse = " = qcv(),\n    "), " = qcv()")), "\n)\n"))
     dimnames(zz) <- list(c(rep("", x)), c(""))
     if (Sys.info()["sysname"] == "Windows") {
         writeClipboard(noquote(rbind(zz, "", "", x1)), format = 1)                        
@@ -61,7 +61,7 @@ function(codes, start = ":00", end, file=NULL) {
         cat(paste0("list(\n",
             "    transcript_time_span = qcv(00:00 - 00:00),\n",
             paste0("    ", paste0(paste(codes, 
-            collapse = " = c(),\n    "), " = c()")), "\n)\n"),
+            collapse = " = qcv(),\n    "), " = qcv()")), "\n)\n"),
             file = file, append = TRUE) 
     }   
     options(width=wid)
