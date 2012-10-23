13a30bb1423492b24060fc6561222cf6c21bed5a
tyler rinker <tyler_rinker@hotmail.com>
2012-10-22 21:43:23 -0400

    added documentation for word and character count

--- R/character.count.R
+++ R/character.count.R
@@ -1,52 +1,23 @@
-#' Transcript Apply Count Number of Characters Used
+#' Count Number of Characters
 #' 
-#' %% ~~ A concise (1-5 lines) description of what the function does. ~~
+#' Transcript Apply Character Counts
 #' 
-#' %% ~~ If necessary, more details than the description above ~~
-#' 
-#' @param text %% ~~Describe \code{text} here~~
-#' @param by %% ~~Describe \code{by} here~~
-#' @param missing %% ~~Describe \code{missing} here~~
-#' @param apostrophe %% ~~Describe \code{apostrophe} here~~
-#' @param digit.remove %% ~~Describe \code{digit.remove} here~~
-#' @return %% ~Describe the value returned %% If it is a LIST, use %%
-#' \item{comp1 }{Description of 'comp1'} %% \item{comp2 }{Description of
-#' 'comp2'} %% ...
-#' @note %% ~~further notes~~
-#' @author %% ~~who you are~~
-#' @seealso %% ~~objects to See Also as \code{\link{help}}, ~~~
-#' @references %% ~put references to the literature/web site here ~
-#' @keywords ~kwd1 ~kwd2
+#' @param text.var The text variable
+#' @param by an optional character string giving a method for counting words.  
+#' This is one of the strings ``row'' or ``all''.
+#' @param missing Value to insert for missing values (empty cells).
+#' @param apostrophe logical.  If TRUE apostrophes will be counted in the character count.
+#' @param digit.remove logical.  If TRUE removes digits before counting words.
+#' @return Returns a character count by row or total.
+#' @seealso \code[qdap]{\link{word.count}}
+#' @keywords character count
 #' @examples
-#' 
-#' ##---- Should be DIRECTLY executable !! ----
-#' ##-- ==>  Define data, use random,
-#' ##--	or do  help(data=index)  for the standard data sets.
-#' 
-#' ## The function is currently defined as
-#' function (text, by = "row", missing = NA, apostrophe = TRUE, 
-#'     digit.remove = TRUE) 
-#' {
-#'     text2 <- if (apostrophe == TRUE) {
-#'         text
-#'     }
-#'     else {
-#'         gsub("'", "", text, fixed = TRUE)
-#'     }
-#'     chara <- function(x) {
-#'         y <- unlist(strsplit(strip(x, digit.remove = digit.remove), 
-#'             NULL))
-#'         z <- subset(y, y != " ")
-#'         length(z)
-#'     }
-#'     OP <- switch(by, all = chara(paste(unlist(text2), collapse = "  ")), 
-#'         row = unlist(lapply(text2, function(x) chara(x))))
-#'     ifelse(OP == 0, missing, OP)
-#'   }
-#' 
+#' character.count(DATA$state)
+#' character.count(DATA$state, by= "all")
+#' sum(character.count(DATA$state))
 character.count <-
-function(text, by = "row", missing = NA, 
-    apostrophe = TRUE, digit.remove = TRUE) {
+function(text.var, by = "row", missing = NA, apostrophe = TRUE, 
+    digit.remove = TRUE) {
     text2 <- if (apostrophe == TRUE) {
         text
     } else {
--- R/word.count.R
+++ R/word.count.R
@@ -1,56 +1,25 @@
-#' Transcript Apply Word Counts
+#' Word Counts
 #' 
 #' Transcript Apply Word Counts
-#' 
-#' %% ~~ If necessary, more details than the description above ~~
 #' 
 #' @aliases word.count wc
-#' @param text %% ~~Describe \code{text} here~~
-#' @param by %% ~~Describe \code{by} here~~
-#' @param missing %% ~~Describe \code{missing} here~~
-#' @param digit.remove %% ~~Describe \code{digit.remove} here~~
-#' @param names %% ~~Describe \code{digit.remove} here~~
-#' @return %% ~Describe the value returned %% If it is a LIST, use %%
-#' \item{comp1 }{Description of 'comp1'} %% \item{comp2 }{Description of
-#' 'comp2'} %% ...
-#' @note %% ~~further notes~~
-#' @author %% ~~who you are~~
-#' @seealso %% ~~objects to See Also as \code{\link{help}}, ~~~
-#' @references %% ~put references to the literature/web site here ~
-#' @keywords ~kwd1 ~kwd2
+#' @param text.var The text variable
+#' @param by an optional character string giving a method for counting words.  
+#' This is one of the strings ``row'' or ``all''.
+#' @param missing Value to insert for missing values (empty cells).
+#' @param digit.remove logical.  If TRUE removes digits before counting words.
+#' @param names logical.  If TRUE the sentences are given as the names of the counts.
+#' @return Returns a word count by row or total.
+#' @seealso \code[qdap]{\link{character.count}}
+#' @keywords word count
 #' @examples
-#' 
-#' ##---- Should be DIRECTLY executable !! ----
-#' ##-- ==>  Define data, use random,
-#' ##--	or do  help(data=index)  for the standard data sets.
-#' 
-#' ## The function is currently defined as
-#' function(text, by = "row", missing = NA, 
-#'     digit.remove = TRUE, names = TRUE) {
-#'     txt <- sapply(as.character(text), function(x) {
-#'             ifelse(is.na(x)|is.null(x), "", x)
-#'         }
-#'     )
-#'     OP <- switch(by, 
-#'          all = {length(unblanker(unlist(word.split(
-#'                    reducer(unlist(strip(
-#'                    paste(as.character(txt), collapse = " "), 
-#'                    digit.remove = digit.remove)))))))}, 
-#'          row = {sapply(txt, function(x) length(unblanker(
-#'                    unlist(word.split(
-#'                    reducer(unlist(strip(as.character(x),
-#'                    digit.remove = 
-#'                    digit.remove))))))))}
-#'     )
-#'     z <- ifelse(OP==0, missing, OP)
-#'     if(!names) names(z) <- NULL
-#'     return(z)
-#' }
-#' 
+#' word.count(DATA$state)
+#' word.count(DATA$state, names = TRUE)
+#' word.count(DATA$state, by= "all", names = TRUE)
+#' sum(word.count(DATA$state))
 word.count <- 
-function(text, by = "row", missing = NA, 
-    digit.remove = TRUE, names = TRUE) {
-    txt <- sapply(as.character(text), function(x) {
+function(text.var, by = "row", missing = NA, digit.remove = TRUE, names = FALSE) {
+    txt <- sapply(as.character(text.var), function(x) {
             ifelse(is.na(x)|is.null(x), "", x)
         }
     )
@@ -60,11 +29,13 @@ function(text, by = "row", missing = NA,
                    digit.remove = digit.remove)))))))}, 
          row = {sapply(txt, function(x) length(unblanker(unlist(word.split(
                    reducer(unlist(strip(as.character(x), digit.remove = 
-         digit.remove))))))))}
+                   digit.remove))))))))}
     )
     z <- ifelse(OP==0, missing, OP)
-    if(!names) names(z) <- NULL
-    return(z)
+    if(!names) {
+        names(z) <- NULL
+    }
+    z
 }
 
 wc <- word.count
