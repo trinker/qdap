fb6fcbaed0cf906653036fda7f9ce4957428c8d1
tyler rinker <tyler_rinker@hotmail.com>
2012-10-24 13:47:00 -0400

    option unblank = T

--- R/qcv.R
+++ R/qcv.R
@@ -1,12 +1,15 @@
 qcv <- 
 function(..., terms = NULL, space.wrap = FALSE, trailing = FALSE, 
-    leading = FALSE, split = " "){
+    leading = FALSE, split = " ", rm.blank = TRUE){
     if (!is.null(terms)) {
         x <- strsplit(terms, split = split)
     } else {
         x <- substitute(...())
     }
     z <- unblanker(scrubber(unlist(lapply(x, function(y) as.character(y)))))
+    if (rm.blank) {
+        z <- unblanker(z)
+    }
     if (space.wrap){
         z <- spaste(z)
     }      
