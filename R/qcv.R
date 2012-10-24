67432d6ad74f46a899021f05eb8168f8371b0a51
tyler rinker <tyler_rinker@hotmail.com>
2012-10-24 13:45:01 -0400

    added unblanker in a correct spot

--- R/qcv.R
+++ R/qcv.R
@@ -2,11 +2,11 @@ qcv <-
 function(..., terms = NULL, space.wrap = FALSE, trailing = FALSE, 
     leading = FALSE, split = " "){
     if (!is.null(terms)) {
-        x <- unblanker(strsplit(terms, split = split))
+        x <- strsplit(terms, split = split)
     } else {
         x <- substitute(...())
     }
-    z <- clean(Trim(unlist(lapply(x, function(y) as.character(y)))))
+    z <- unblanker(scrubber(unlist(lapply(x, function(y) as.character(y)))))
     if (space.wrap){
         z <- spaste(z)
     }      
