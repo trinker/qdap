2e3b2f85d915182bb76b13989bbb515c948f3fe7
tyler rinker <tyler_rinker@hotmail.com>
2012-10-24 13:39:43 -0400

    unblankered qcv

--- R/qcv.R
+++ R/qcv.R
@@ -1,15 +1,15 @@
 qcv <- 
-  function(..., terms = NULL, space.wrap = FALSE, trailing = FALSE, 
+function(..., terms = NULL, space.wrap = FALSE, trailing = FALSE, 
     leading = FALSE, split = " "){
-  if (!is.null(terms)) {
-    x <- strsplit(terms, split = split)
-  } else {
-    x <- substitute(...())
-  }
-  z <- clean(Trim(unlist(lapply(x, function(y) as.character(y)))))
-  if (space.wrap){
-    z <- spaste(z)
-  }      
-  z <- spaste(z, trailing = trailing, leading = leading) 
-  return(z)
+    if (!is.null(terms)) {
+        x <- unblanker(strsplit(terms, split = split))
+    } else {
+        x <- substitute(...())
+    }
+    z <- clean(Trim(unlist(lapply(x, function(y) as.character(y)))))
+    if (space.wrap){
+        z <- spaste(z)
+    }      
+    z <- spaste(z, trailing = trailing, leading = leading) 
+    z
 }
