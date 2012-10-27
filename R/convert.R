a963f6dd2b07d78769307aafd91dc9b94b4a0b02
tyler rinker <tyler_rinker@hotmail.com>
2012-10-26 23:26:39 -0400

    added internal convert

---
+++ R/convert.R
@@ -0,0 +1,18 @@
+convert <-
+function(x, l1 = FALSE) {
+        if (length(x) == 1) {
+            x <- c(x, 0)
+            l1 <- TRUE
+        }
+        h <- floor(x/3600)
+        m <- floor((x-h*3600)/60)
+        s <- x-(m*60 + h*3600)
+        pad <- function(x) sprintf("%02d", as.numeric(x))
+        op <- times(paste2(data.frame(apply(data.frame(h=h, m=m, s=s), 
+            2, pad)), sep=":"))
+        if (l1) {
+            op[1]
+        } else {
+            op
+        }
+    }
