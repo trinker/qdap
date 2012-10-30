2ed3f93fd8994b930ec90d264334e35a743ac1bf
tyler rinker <tyler_rinker@hotmail.com>
2012-10-30 09:46:25 -0400

    added bidist, describe and proximaty and exported for now to create cm_distance

---
+++ R/cm_bidist.R
@@ -0,0 +1,32 @@
+cm_bidist <-
+function(code_x, code_y, grouping.var = NULL) {
+    x <- cm_describe(code_x, grouping.var)
+    x <- x[as.numeric(as.character(x[, "code_x"])) > 0, ]
+    y <- cm_describe(code_y, grouping.var)
+    y <- y[as.numeric(as.character(y[, "code_y"])) > 0, ]
+    Dnc <- sapply(1:nrow(x), function(i) {
+            yind <- 1:nrow(y)
+            if (sum(y[, "start"] >= x[i, "start"] & y[, "start"] <= x[i, "end"]) > 0 |
+                sum(y[, "end"] >= x[i, "start"] & y[, "end"] <= x[i, "start"]) > 0 |
+                sum(sapply(yind, function(j) {
+                        y[j, "start"] < x[i, "start"] & y[j, "end"] > x[i, "end"]
+                    }
+                )) > 0) {
+                return(0)
+            }
+            sdif <- c(y[, "start"], y[, "end"]) - x[i, "start"]
+            edif <- c(y[, "start"], y[, "end"]) - x[i, "end"]
+            min(abs(c(sdif, edif)))
+        }
+    )
+    Dc <- sapply(1:nrow(x), function(i) {
+        FUN <- function(xstart, xend, ystart){
+             max(0, min(ystart[ystart > xstart] - xend))
+        }
+        suppressWarnings(FUN(x[i, "start"], x[i, "end"], y[, "start"]))
+        }
+    )
+    Dc[is.infinite(Dc)] <- NA
+    list(associated_distance = Dnc, mean.sd_assoc_dist = c(mean(Dnc), sd(Dnc)), 
+        causal_distance = Dc, mean.sd_causal_dist = c(mean(na.omit(Dc)), sd(na.omit(Dc))))
+}
---
+++ R/cm_describe.R
@@ -0,0 +1,55 @@
+cm_describe <-
+function(code, grouping.var = NULL) {
+    if (!is.null(grouping.var)) {
+        if (is.list(grouping.var)) {
+          m <- unlist(as.character(substitute(grouping.var))[-1])
+          m <- sapply(strsplit(m, "$", fixed = TRUE), 
+                      function(x) x[length(x)])
+          NAME <- paste(m, collapse = "&")
+        } else {
+          G <- as.character(substitute(grouping.var))
+          NAME <- G[length(G)]
+        }
+        cname <- strsplit(as.character(substitute(code)), "&")
+        NAME <- paste0(cname[[length(cname)]], "&", NAME)
+        group.var <- if (is.list(grouping.var) & length(grouping.var)>1) {
+          apply(data.frame(grouping.var), 1, function(x){
+            if (any(is.na(x))){
+              NA
+            } else {
+              paste(x, collapse = ".")
+            }
+          }
+          )
+        } else {
+          grouping.var
+        }
+        v <- do.call(data.frame, rle(paste2(list(code, group.var))))
+    } else {
+        v <- do.call(data.frame, rle(code))
+    }
+    v$end<- cumsum(v[, 1])
+    colnames(v)[1] <- "duration"
+    v$start <- c(0, c(v$end)[-c(length(v$end))])
+    v$center <- (v$start + v$end)/2
+    v2 <- v[, c("values", "center", "duration", "start", "end")]
+    if (!is.null(grouping.var)) {
+        nl <- if (is.list(grouping.var)) {
+            grouping.var 
+        } else { 
+            list(grouping.var)
+        }
+        L2 <- lapply(1:(length(nl) + 1), function(i) {
+            x <- strsplit(as.character(v2[, "values"]), "\\.")
+            sapply(1:length(x), function(j)x[[j]][i])
+            }
+        )
+        v3 <- data.frame(do.call(cbind, L2))
+        colnames(v3) <- unlist(strsplit(NAME, "\\&"))
+        v2 <- data.frame(v3, v2[, -1, drop=FALSE])      
+    } else {
+        cname <- strsplit(as.character(substitute(code)), "&")
+        colnames(v2)[1] <- cname[[length(cname)]]
+    }
+    return(v2)
+}
