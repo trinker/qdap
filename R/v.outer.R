f8c4553d827435af44ece7bf07799f6c52a50e9e
tyler rinker <tyler_rinker@hotmail.com>
2012-10-30 00:04:54 -0400

    added v.outer to adress issue #72

---
+++ R/v.outer.R
@@ -0,0 +1,11 @@
+v.outer <-
+function(x, FUN){
+    FUN <- match.fun(FUN)
+    z <- outer(
+      colnames(x), 
+      colnames(x), 
+      Vectorize(function(i,j) FUN(x[,i], x[,j]))
+    )
+    dimnames(z) <- list(colnames(x), colnames(x))
+    z
+}
