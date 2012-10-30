fc21e6d140a0d85f76ef6e4da29d01319183a80c
tyler rinker <tyler_rinker@hotmail.com>
2012-10-30 00:53:54 -0400

    missed a parenthesis

--- R/v.outer.R
+++ R/v.outer.R
@@ -6,7 +6,7 @@ v.outer <- function(x, FUN, digits = 3, ...){
       Vectorize(function(i,j) FUN(x[,i], x[,j], ...))
     )
     dimnames(z) <- list(colnames(x), colnames(x))
-    if (is.numeric(z) {
+    if (is.numeric(z)) {
         z <- round(z, digits = digits)
     }
     z
