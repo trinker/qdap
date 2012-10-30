98484048145534ef5c710ce8636edf699549f88e
tyler rinker <tyler_rinker@hotmail.com>
2012-10-30 01:45:38 -0400

    added ability to use list or matrix or data frame

--- R/v.outer.R
+++ R/v.outer.R
@@ -1,9 +1,13 @@
-v.outer <- function(x, FUN, digits = 3, ...){
+v.outer <- 
+function(x, FUN, digits = 3, ...){
     FUN <- match.fun(FUN)
+    if (is.matrix(x)) {
+        x <- as.data.frame(x)
+    }
     z <- outer(
       colnames(x), 
       colnames(x), 
-      Vectorize(function(i,j) FUN(x[,i], x[,j], ...))
+      Vectorize(function(i,j) FUN(x[[i]], x[[j]], ...))
     )
     dimnames(z) <- list(colnames(x), colnames(x))
     if (is.numeric(z)) {
