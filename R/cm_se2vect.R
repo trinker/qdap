432f7d9f5bae639de4f32886b0c709b5dc843695
tyler rinker <tyler_rinker@hotmail.com>
2012-11-02 03:13:44 -0400

    added a function to turn start end times into a single vector of dummy coded values

---
+++ R/cm_se2vect.R
@@ -0,0 +1,16 @@
+cm_se2vect <-
+function(start, end, n.words = NULL, safety = TRUE) {
+    dat <- data.frame(start =start, end = end)
+    if (is.null(n.words)){
+        n.words <- max(dat$end) - 1
+    }
+    if (safety) {
+        y <- dat[, "start"]
+        if(any(sapply(seq_along(y)[-length(y)], function(i) y[i] > y[i+1]))){
+            stop("each element of the start column is not growing")
+        }
+    }
+    x <- rep(0, n.words)
+    x[unlist(lapply(1:nrow(dat), function(i) dat$start[i]:(dat$end[i]-1)))] <- 1
+    x
+}
