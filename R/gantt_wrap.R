4a84e2a434d340cb666d5ff20507f4abeb8607e4
tyler rinker <tyler_rinker@hotmail.com>
2012-11-04 00:26:33 -0400

    added -2 back b/c there's an issue with time stamps now

--- R/gantt_wrap.R
+++ R/gantt_wrap.R
@@ -52,7 +52,7 @@ gantt_wrap <-
 function(dataframe, plot.var, facet.vars = NULL, fill.var = NULL, title = NULL, 
     ylab = as.character(plot.var), xlab = "duration.default", rev.factor = TRUE,
     transform = FALSE, ncol = NULL, minor.line.freq = NULL, 
-    major.line.freq = NULL, sig.dig.line.freq = 1, hms.scale = NULL, 
+    major.line.freq = NULL, sig.dig.line.freq = -2, hms.scale = NULL, 
     scale = NULL, space = NULL, size = 3, rm.horiz.lines = FALSE, x.ticks = TRUE, 
     y.ticks = TRUE, legend.position = NULL, bar.color = NULL,
     border.color = NULL, border.size = 2, border.width = .1, constrain = TRUE) { 
