31070ab8a618b4a389cdc929869faa0cf9517368
tyler rinker <tyler_rinker@hotmail.com>
2012-11-05 23:40:12 -0500

    debugged cm_distance hopefully for good.

--- R/cm_distance.R
+++ R/cm_distance.R
@@ -52,68 +52,58 @@ cm_distance <-
 function(dataframe, time.var = NULL, code.var = "code",
     causal = FALSE, start.var = "start", end.var = "end", mean.digits = 2, 
     sd.digits = 2, stan.digits = 2) {
+    DIST <- function(DF, CV= code.var, 
+        CAU = causal, SV = start.var, EV = end.var, 
+        MD = mean.digits, SDD = sd.digits,  SD = stan.digits) {  
+        L2 <- split(DF, DF[, CV])
+        L2 <- L2[sapply(L2, nrow) != 0] 
+        NMS <- names(L2) 
+        L3 <- lapply(seq_along(L2), function(i) {
+            cm_se2vect(L2[[i]][, SV], L2[[i]][, EV])
+        })
+        lens <- max(sapply(L3, length))
+        L4 <- lapply(seq_along(L3), function(i){
+            c(L3[[i]], rep(0, lens - length(L3[[i]])))
+        })
+        dat <- do.call(cbind, L4)  
+        colnames(dat) <- NMS
+        if (CAU) {inds <- 4} else {inds <- 2}
+        dism <- function(x, y) {cm_bidist(x, y)[[inds]][1]}
+        dissd <- function(x, y) {cm_bidist(x, y)[[inds]][2]}
+        disn <- function(x, y) {cm_bidist(x, y)[[inds]][3]}
+        FUN <- function(dat, mdigs=MD, sdigs=SDD){
+            dat <- data.frame(dat)
+            means <- v.outer(dat, dism, digits=mdigs)
+            sds <- v.outer(dat, dissd, digits=sdigs)
+            ns <- v.outer(dat, disn, digits=0)
+            DIM <- dim(means)
+            pfun <- function(x, y, z) paste0(x, "(", y, ")", "n=", z)
+            comb <- mgsub(c("(NA)", "NA;n=0"), c(";", NA), 
+                mapply(pfun, means, sds, ns), fixed=TRUE)
+            dim(comb) <- DIM
+            dimnames(comb) <- list(rownames(means), colnames(means))
+            diag(comb) <- gsub("0(0)", "", diag(comb), fixed=TRUE)
+            scale.all <- function(x) {
+                dims <- dim(x)
+                dnms <- dimnames(x)
+                x <- matrix(scale(c(x), F), dims)
+                dimnames(x) <- dnms
+                x
+            }
+            stand <- round(scale.all(means)*scale.all(sds), 
+                digits=SD)
+            stand[is.nan(stand)] <- NA
+            list(mean=means, sd=sds, n=ns, combined=noquote(comb), standardized=stand)
+        }
+        FUN(dat)
+    }  
     if (!is.null(time.var)) {
         L1 <- split(dataframe, dataframe[, time.var])
     } else {
         L1 <- list(dataframe)
         names(L1) <- as.character(substitute(dataframe))
     }   
-    L2 <- lapply(L1, function(x) split(x, x[, code.var]))   
-    NMS <- lapply(L2, names)
-    NMS <- unlist(lapply(seq_along(NMS), function(i) {
-        paste(names(NMS)[i], NMS[[i]], sep=".")
-    }))
-    lens <- sapply(L2, length)
-    mlens <- sapply(L2, function(x) {
-        max(do.call(rbind, x)[, end.var])
-    })
-    nt <- rep(mlens, lens)
-    v <- unlist(L2, recursive=FALSE)
-    L3 <- lapply(seq_along(v), function(i) {
-        cm_se2vect(v[[i]][, start.var], v[[i]][, end.var])
-    })
-    L4 <- lapply(seq_along(L3), function(i){
-        c(L3[[i]], rep(0, nt[i] - length(L3[[i]])))
-    })
-    dat <- do.call(cbind, L4)  
-    colnames(dat) <- NMS
-    INDS <- cumsum(lens)
-    INDS2 <- c(1, INDS +1); INDS2 <- INDS2[-length(INDS2)] 
-    L5 <- lapply(seq_along(INDS), function(i) dat[, INDS2[i]:INDS[i]])
-    names(L5) <- names(L2)
-    L5 <- lapply(L5, function(x){
-        colnames(x) <- unlist(strsplit(colnames(x), "\\."))[c(F, T)]
-        return(x)
-    })
-    if (causal) {inds <- 4} else {inds <- 2}
-    dism <- function(x, y) {cm_bidist(x, y)[[inds]][1]}
-    dissd <- function(x, y) {cm_bidist(x, y)[[inds]][2]}
-    disn <- function(x, y) {cm_bidist(x, y)[[inds]][3]}
-    FUN <- function(dat, mdigs=mean.digits, sdigs=sd.digits){
-        dat <- data.frame(dat)
-        means <- v.outer(dat, dism, digits=mdigs)
-        sds <- v.outer(dat, dissd, digits=sdigs)
-        ns <- v.outer(dat, disn, digits=0)
-        DIM <- dim(means)
-        pfun <- function(x, y, z) paste0(x, "(", y, ")", "n=", z)
-        comb <- mgsub(c("(NA)", "NA;n=0"), c(";", NA), 
-            mapply(pfun, means, sds, ns), fixed=TRUE)
-        dim(comb) <- DIM
-        dimnames(comb) <- list(rownames(means), colnames(means))
-        diag(comb) <- gsub("0(0)", "", diag(comb), fixed=TRUE)
-        scale.all <- function(x) {
-            dims <- dim(x)
-            dnms <- dimnames(x)
-            x <- matrix(scale(c(x), F), dims)
-            dimnames(x) <- dnms
-            x
-        }
-        stand <- round(scale.all(means)*scale.all(sds), 
-            digits=stan.digits)
-        stand[is.nan(stand)] <- NA
-        list(mean=means, sd=sds, n=ns, combined=noquote(comb), standardized=stand)
-    }
-    o <- lapply(L5, FUN)
+    o <- lapply(L1, DIST)
     class(o) <- "cm.dist"
     return(o)
 }
