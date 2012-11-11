e458268260266065ce032caf46c10b13b486d8cf
tyler rinker <tyler_rinker@hotmail.com>
2012-11-11 17:20:23 -0500

    didn't upload export(cm_code.transfrm)

---
+++ R/cm_code.transform.R
@@ -0,0 +1,77 @@
+#' Transform Codes
+#' 
+#' Transform co-occurences and/or combinations of codes into a new code(s).
+#' 
+#' @param x2long.obj An object from cm_range2long, cm_time2long or cm_df2long
+#' @param overlap.code.list A list of named character vertors of at least two code column names to aggregate co-occurences.
+#' @param overlap.code.list A list of named character vertors of at least two code column names to combine
+#' @param rm.var Name of the repeated measures column.  
+#' @return Returns a dataframe with and/or overlapping combined occurrences of supplied overlapping codes added.
+#' @note The code column must be named code and your start and end columns must be named "start" and "end".
+#' @seealso \code{\link[qdap]{cm_range2long}},
+#' \code{\link[qdap]{cm_time2long}},
+#' \code{\link[qdap]{cm_df2long}},
+#' \code{\link[qdap]{cm_code.combine}},
+#' \code{\link[qdap]{cm_code.overlap}}
+#' @keywords transform
+#' @examples
+#' foo <- list(
+#'     AA = qcv(terms='1:10'),
+#'     BB = qcv(terms='1:2, 3:10, 19'),
+#'     CC = qcv(terms='1:3, 5:6')
+#' )
+#' 
+#' foo2  <- list(
+#'     AA = qcv(terms='4:8'),
+#'     BB = qcv(terms='1:4, 10:12'),
+#'     CC = qcv(terms='1, 11, 15:20'),
+#'     DD = qcv(terms='')
+#' )
+#' 
+#' x <- cm_range2long(foo)
+#' z <- cm_range2long(foo, foo2, v.name="time")
+#' overlaps <- list(AB=qcv(AA, BB), ABC=qcv(AA, BB, CC))
+#' cm_code.transform(x, overlap.code.list=list(AB=qcv(AA, BB)))
+#' cm_code.transform(x, combine.code.list = list(ALL=qcv(AA, BB, CC)))
+#' cm_code.transform(x, overlap.code.list=list(AB=qcv(AA, BB)), 
+#'     combine.code.list = list(ALL=qcv(AA, BB, CC)))
+#' cm_code.transform(z, overlaps, combine.code.list=NULL, "time")
+#' 
+#' #WITH cm_time2long
+#' x <- list(
+#'     transcript_time_span = qcv(00:00 - 1:12:00),
+#'     A = qcv(terms = "2.40:3.00, 5.01, 6.02:7.00, 9.00"),
+#'     B = qcv(terms = "2.40, 3.01:3.02, 5.01, 6.02:7.00, 9.00, 1.12.00:1.19.01"),
+#'     C = qcv(terms = "2.40:3.00, 5.01, 6.02:7.00, 9.00, 17.01")
+#' )
+#' 
+#' y <- list(
+#'     transcript_time_span = qcv(00:00 - 1:12:00),
+#'     A = qcv(terms = "2.40:3.00, 5.01, 6.02:7.00, 9.00"),
+#'     B = qcv(terms = "2.40, 3.01:3.02, 5.01, 6.02:7.00, 9.00, 1.12.00:1.19.01"),
+#'     C = qcv(terms = "2.40:3.00, 5.01, 6.02:7.00, 9.00, 17.01")
+#' )
+#' 
+#' dat <- cm_time2long(x, y)
+#' cm_code.transform(dat, list(P=qcv(A, B), Q=qcv(B, C), R=qcv(A, B, C)), 
+#'     list(S=qcv(A, B), T=qcv(B, C), U=qcv(A, B, C)), rm.var="variable")
+cm_code.transform <- function(x2long.obj, overlap.code.list=NULL,
+    combine.code.list=NULL,  rm.var = NULL) {
+    A <- B <- NULL
+    if (!is.null(overlap.code.list)){
+        A <- cm_code.overlap(x2long.obj = x2long.obj, 
+            overlap.code.list = overlap.code.list, rm.var = rm.var)
+    }
+    if (!is.null(combine.code.list)){
+        B <- cm_code.combine(x2long.obj = x2long.obj, 
+            combine.code.list = combine.code.list, rm.var = rm.var)
+    }
+    DF <- data.frame(rbind(A, B))
+    DF <- DF[!duplicated(DF), ]
+    rownames(DF) <- NULL
+    if (!is.null(rm.var)) {
+        DF <- DF[order(DF[, rm.var]), ]
+    }
+    DF
+}
+
