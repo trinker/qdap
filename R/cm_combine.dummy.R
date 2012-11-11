e3530955a9adffba7ea38cc7e09edebac100b70b
tyler rinker <tyler_rinker@hotmail.com>
2012-11-10 19:47:10 -0500

    added cm_combine.dummy to address issue #77

---
+++ R/cm_combine.dummy.R
@@ -0,0 +1,55 @@
+#' Find Co-occurrence Between Codes
+#'
+#' Combine code columns where they co-occur.
+#'
+#' @param cm.l2d.obj An object from cm_long2dummy
+#' @param combine.code A list of named character vertors of at least two code column names to combine
+#' @param rm.var Name of the repeated measures column.  Default is "time".
+#' @return Returns a dataframe with co-occurrences of provided code columns.
+#' @seealso \code{\link[qdap]{cm_long2dummy}}
+#' @keywords co-occurence
+#' @examples
+#' foo <- list(
+#'     AA = qcv(terms='1:10'),
+#'     BB = qcv(terms='1:2, 3:10, 19'),
+#'     CC = qcv(terms='1:3, 5:6')
+#' )
+#' 
+#' foo2  <- list(
+#'     AA = qcv(terms='4:8'),
+#'     BB = qcv(terms='10:12'),
+#'     CC = qcv(terms='1, 11, 15:20'),
+#'     DD = qcv(terms='')
+#' )
+#' 
+#' x <- cm_range2long(foo)
+#' D1 <- cm_long2dummy(x)
+#' 
+#' z <- cm_range2long(foo, foo2, v.name="time")
+#' D2 <- cm_long2dummy(z, "time")
+#' cm_combine.dummy(D1, combine.code = list(AB=qcv(AA, BB)))
+#' 
+#' combines <- list(AB=qcv(AA, BB), ABC=qcv(AA, BB, CC))
+#' cm_combine.dummy(D1, combine.code = combines)
+#' cm_combine.dummy(D2, combine.code = combines)
+cm_combine.dummy <- function(cm.l2d.obj, combine.code, rm.var = "time") {
+    if (is.data.frame(cm.l2d.obj)) {
+        NMS <- as.character(substitute(cm.l2d.obj))
+        cm.l2d.obj <- list(cm.l2d.obj)
+        names(cm.l2d.obj) <- NMS
+    }
+    invisible(lapply(seq_along(cm.l2d.obj), function(i) {
+        cm.l2d.obj[[i]][, rm.var] <<- names(cm.l2d.obj)[i]
+    }))
+    DF <- data.frame(do.call(rbind, cm.l2d.obj), row.names=NULL)
+    if (!is.list(combine.code)) {
+        combine.code <- list(combine.code)
+    }
+    NEW <- lapply(seq_along(combine.code), function(i) {
+        as.numeric(rowSums(DF[, c(combine.code[[i]])]) == length(combine.code[[i]]))
+    })
+    names(NEW) <- names(combine.code)
+    tv <- ncol(DF)
+    data.frame(DF[, -tv, drop=FALSE], do.call(cbind, NEW), DF[, tv, drop=FALSE])
+}
+
