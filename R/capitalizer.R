910e3fdfe1144cca3fdce0ee9c1a587268613483
tyler rinker <tyler_rinker@hotmail.com>
2012-11-24 15:03:50 -0500

    altered capitalizer for more general use but may break code elswhere

--- R/capitalizer.R
+++ R/capitalizer.R
@@ -5,31 +5,30 @@
 #' @param text A vector of words (generally from bag.o.words or breaker).
 #' @param caps.list A list of words to capitalize.
 #' @param I.list logical.  If TRUE capitalizes I words and contractions.
-#' @param no.apostrophe logical.  If TRUE will not insert apostrophe's back into words.
+#' @param no.apostrophe logical, asking if apostrophes have been removed.  If TRUE will try to insert apostrophe's back into words appropriately.
 #' @return Returns a vector of capitalized words based on supplied capitalization arguments.
 #' @note Not intended for general use.  Acts as a helper function to several qdap functions.
 #' @examples
 #' capitalizer(bag.o.words("i like it but i'm not certain"), "like")
-#' capitalizer(bag.o.words("i like it but i'm not certain"), "like", F)
+#' capitalizer(bag.o.words("i like it but i'm not certain"), "like", FALSE)
 capitalizer <-
-function(text, caps.list = NULL, I.list = TRUE, no.apostrophe = FALSE) {
+function(text, caps.list = NULL, I.list = TRUE, apostrophe.remove = FALSE) {
     I_list <- c("I'm", "I'll", "I'd", "I've", "I")
     IDF <- data.frame(from1 = sapply(I_list, function(x) strip(x, 
-        apostrophe.remove = TRUE)), from2 = sapply(I_list, strip), 
+        apostrophe.remove = FALSE)), from2 = sapply(I_list, strip), 
         to = I_list)
-    
-    idf <- if (no.apostrophe == FALSE) {
-        IDF[-1]
+    if (apostrophe.remove) {
+        idf <- IDF[-1]
     } else {
         IDF2 <- IDF[-2]
         names(IDF2) <- c("from2", "to")
-        data.frame(rbind(IDF[-1], IDF2))
+        idf <- data.frame(rbind(IDF[-1], IDF2))
     }
     names(idf) <- c("from", "to")
     rownames(idf) <- 1:nrow(idf)
-    
-    idf <- if (I.list) 
-        idf else NULL
+    if (!I.list) {
+        idf <- NULL
+    }
     names <- data.frame(from = tolower(caps.list), to = gsub("(\\w)(\\w*)", 
         "\\U\\1\\L\\2", tolower(caps.list), perl = T))
     names2 <- data.frame(from = paste(names$from, "'s", sep = ""), 
