5254f747489ff719da7bb603b59e6e61e3a9049b
tyler rinker <tyler_rinker@hotmail.com>
2012-11-24 14:18:55 -0500

    does not remove apostrophes automatically anymore

--- R/bag.o.words.R
+++ R/bag.o.words.R
@@ -3,6 +3,7 @@
 #' Reduces a text column to a bag of words.
 #' 
 #' @param text.var The text variable
+#' @param apostrophe.remove logical.  If TRUE removes apostrophe's from the output
 #' @param \\ldots further arguments passed to strip function
 #' @return Returns a vector of striped words.
 #' @seealso \code{\link[qdap]{strip}}, \code{\link[qda]{breaker}}
@@ -13,4 +14,6 @@
 #' lapply(DATA$state,  bag.o.words)
 #' bag.o.words("I'm going home!", apostrophe.remove = FALSE)
 bag.o.words <-
-function(text.var, ...) unblanker(words(strip(clean(text.var), ...)))
+function(text.var, apostrophe.remove = FALSE, ...) {
+    unblanker(words(strip(clean(text.var), apostrophe.remove = apostrophe.remove, ...)))
+}
