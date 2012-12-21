81c3c2d318c1e4acebddd25d790bdbc74a514d66
tyler rinker <tyler_rinker@hotmail.com>
2012-12-21 01:19:32 -0500

    combined the two bracket functions together

--- R/bracketX.R
+++ R/bracketX.R
@@ -1,16 +1,17 @@
-#' Removed Bracketted Text
+#' Bracket Parsing
 #' 
-#' Transcript Apply Removal of Brackets and Encased Text
+#' \code{bracketX} - Apply bracket removal to character vectors.
 #' 
 #' @param text.var The text variable
 #' @param bracket The type of bracket (and encased text) to remove.  This is one of 
-#' the strings "curly", "square", "round", "angle" and "all".  
-#' These strings correspond to: {, [, (, < or all four types.
+#' the strings \code{"curly"}, \code{"square"}, \code{"round"}, \code{"angle"} 
+#' and \code{"all"}.  These strings correspond to: \{, [, (, < or all four types.
 #' @param missing Value to assign to empty cells.
 #' @param names logical.  If TRUE the sentences are given as the names of the counts.
-#' @return Returns a vector of text with brackets removed.
-#' @seealso \code[qdap]{\link{bracketXtract}}
-#' @keywords bracket remove
+#' @return bracketX returns a vector of text with brackets removed.
+#' @rdname bracketX
+#' @references \url{http://stackoverflow.com/questions/8621066/remove-text-inside-brackets-parens-and-or-braces}
+#' @keywords bracket-remove, parenthesis, bracket, curly-braces
 #' @export
 #' @examples
 #' examp2 <- examp2 <- structure(list(person = structure(c(1L, 2L, 1L, 3L), 
@@ -20,7 +21,7 @@
 #'     "Yep it's awesome {reading}.", "Agreed. {is so much fun}")), .Names = 
 #'     c("person", "text"), row.names = c(NA, -4L), class = "data.frame")    
 #' 
-#' examp2                                                              
+#' examp1                                                              
 #' bracketX(examp2$text, 'square')  
 #' bracketX(examp2$text, 'curly')  
 #' bracketX(examp2$text)  
@@ -29,6 +30,7 @@
 #' bracketXtract(examp2$text, 'square')  
 #' bracketXtract(examp2$text, 'curly')  
 #' bracketXtract(examp2$text)  
+#' bracketXtract(examp2$text, with = TRUE)
 #' 
 #' paste2(bracketXtract(examp2$text, 'curly'), " ")
 bracketX <-
@@ -52,4 +54,41 @@ function (text.var, bracket = "all", missing = NULL, names = FALSE) {
     }
     if (!names) names(X) <- NULL
     X
+}
+
+#' bracketXtract
+#' 
+#' \code{bracketXtract} - Apply bracket extraction to character vectors.
+#' 
+#' @rdname bracketX
+#' @param with logical.  If TRUE returns the brackets and the bracketted text.
+#' @return bracketXtract returns a list of vectors of bracketed text.
+#' @author  Martin Morgan and Tyler Rinker <tyler.rinker@@gmail.com>.
+#' @export
+bracketXtract <-
+function(text.var, bracket = "all", with = FALSE){   
+    br <- bracket
+    br <- ifelse(br=="round", "(", 
+        ifelse(br=="square", "[", 
+        ifelse(br=="curly", "{",
+        ifelse(br=="html", "<",
+        ifelse(br=="angle", "<", br)))))
+    left <- if ("all" == br) {
+        "\\(|\\{|<|\\["
+    } else {
+        sprintf("\\%s", br)
+    }
+    map <- c(`\\(`="\\)", `\\[`="\\]", `\\{`="\\}",
+             `\\<`="\\>", `\\(|\\{|<|\\[`="\\)|\\}|\\>|\\]")
+    fmt <- if (with==TRUE) {
+        "(%s).*?(%s)"
+    } else {
+        "(?<=%s).*?(?=%s)"
+    }
+    re <- sprintf(fmt, left, map[left])
+    if(length(text.var)==1){
+        unlist(regmatches(text.var, gregexpr(re, text.var, perl=TRUE)))
+    }else{  
+        regmatches(text.var, gregexpr(re, text.var, perl=TRUE)) 
+    }
 }
