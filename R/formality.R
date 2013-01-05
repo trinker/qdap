cc5c91c1cbea0fb8b47b79a327faa7f76295ce33
tyler rinker <tyler_rinker@hotmail.com>
2013-01-05 11:06:06 -0500

    documentation

--- R/formality.R
+++ R/formality.R
@@ -19,7 +19,7 @@
 #' respectively.
 #' @param min.wrdcnt A minimum word count threshold that must be achieved to be 
 #' considered in the results.  Default includes all subgroups.
-#' @warning Heylighen & Dewaele(2002) say "At present, a sample would probably 
+#' @note Heylighen & Dewaele(2002) say "At present, a sample would probably 
 #' need to contain a few hundred words for the measure to be minimally reliable. 
 #' For single sentences, the F-value should only be computed for purposes of 
 #' illustration".
