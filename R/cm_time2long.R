951159b6c7588e31431e489cc8d3dbe54fc85c27
tyler rinker <tyler_rinker@hotmail.com>
2013-01-09 02:57:01 -0500

    added an export

--- R/cm_time2long.R
+++ R/cm_time2long.R
@@ -19,6 +19,7 @@
 #' @references Miles, M. B. & Huberman, A. M. (1994). An expanded sourcebook: 
 #' Qualitative   data analysis. 2nd ed. Thousand Oaks, CA: SAGE Publications.
 #' @keywords coding, time span
+#' @export
 #' @examples
 #' \dontrun{
 #' x <- list(
