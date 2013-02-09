50cc11efd9ef3a14d049f7687f320945d4857aa3
tyler rinker <tyler_rinker@hotmail.com>
2013-02-09 01:03:54 -0500

    added maintenence.R

---
+++ inst/maintenance.R
@@ -0,0 +1,23 @@
+
+#==========
+#staticdocs
+#==========
+#packages
+library(highlight); library(qdap); library(staticdocs); library(acc.roxygen2)
+
+#STEP 1: create static doc
+build_package(package="C:/Users/trinker/GitHub/qdap", 
+    base_path="C:/Users/trinker/Desktop/qdap/", examples = TRUE)
+
+#STEP 2: reshape index
+path <- "C:/Users/trinker/Desktop/qdap"
+path2 <- paste0(path, "/index.html")
+extras <- qcv(right.just, coleman_liau, flesch_kincaid, fry, 
+    linsear_write, SMOG, syn, mgsub, adjmat, wc, char.table, wfdf)
+expand_statdoc(path2, to.icon = extras)
+
+#STEP 3: move to trinker.guthub
+file <- "C:/Users/trinker/GitHub/trinker.github.com/"
+delete(paste0(file, "qdap"))
+file.copy(path, file, TRUE, TRUE)
+delete(path)
