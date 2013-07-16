4cdcd18e66302baf38eea13f8cd8fdb017ecc46c
tyler rinker <tyler_rinker@hotmail.com>
2013-07-15 23:07:57 -0400

    added condense

--- R/condense.R
+++ R/condense.R
@@ -1,17 +1,26 @@
-## Tools to condense lists of vectors stored in dataframes for export to csv/xlsx
-## Not currently exported
+#' Condense Dataframe Columns 
+#' 
+#' Condense dataframe columns that are a list of vectors to a single vector of 
+#' strings.
+#' 
+#' @param dataframe A dataframe with a column(s) that are a list of vectors.
+#' @param sep A character string to separate the terms.
+#' @return Returns a dataframe with condensed columns that can be wrote to 
+#' csv/xlsx.
+#' @export 
+#' @examples
+#' \dontrun{
+#' library(qdap)
+#' poldat <- with(DATA, polarity(state, person))
+#' write.csv(x = condense(poldat$all), file = "foo.csv")
+#' }
+condense <- function(dataframe, sep = ", ") {
+    whichlist <- sapply(dataframe, is.list)
+    dataframe[, whichlist] <- sapply(dataframe[, whichlist], paste3, sep = sep)
+    dataframe
+}
+
 paste3 <- function(x, sep = ", "){
     sapply(x, paste, collapse = sep)
 }
 
-condense <- function(dataframe, ...) {
-    whichlist <- sapply(dataframe, is.list)
-    dataframe[, whichlist] <- sapply(dataframe[, whichlist], paste3, ...)
-    dataframe
-}
-
-## poldat <- with(DATA, polarity(state, person))
-## out1 <- merge(DATA, poldat$all, by.x=qcv(person, state), by.y=qcv(person, text.var))
-## 
-## library(xlsx)
-## write.csv(x = condense(out1), file = "out1.csv")
