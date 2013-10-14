c12521d08448d3fa0d7723516413ee55dad71c93
tyler rinker <tyler.rinker@gmail.com>
2013-10-14 00:43:38 -0400

    added `diag.na` argument

--- R/qheat.R
+++ R/qheat.R
@@ -33,6 +33,10 @@
 #' @param facet.vars A character vector of of 1 or 2 column names to facet by.
 #' @param facet.flip logical If \code{TRUE} the direction of the faceting 
 #' is reversed.
+#' @param diag.na logical.  If \code{TRUE} and \code{mat} is a symetrical 
+#' matrix the diagonals are set to \code{NA}.  This is useful with correlation 
+#' matrices because the diagonal of ones do not effect the scaling of the 
+#' heatmap.
 #' @details \code{qheat} is useful for finding patterns and anomalies in large
 #' qdap generated dataframes and matrices.
 #' @note \code{\link[qdap]{qheat}} is a fast way of working with data formats 
@@ -61,6 +65,7 @@
 #' qheat(mtcars, facet.vars = "cyl")
 #' qheat(mtcars, facet.vars = c("gear", "cyl"))
 #' qheat(t(mtcars), by.column=FALSE)
+#' qheat(cor(mtcars), diag.na=TRUE)
 #' 
 #' dat1 <- data.frame(G=LETTERS[1:5], matrix(rnorm(20), ncol = 4))
 #' dat2 <- data.frame(matrix(LETTERS[1:25], ncol=5))
@@ -72,9 +77,18 @@ function(mat, low = "white", high ="darkblue", values = FALSE,
     digits = 1, text.size = 3, text.color = "grey40", xaxis.col = "black",
     yaxis.col = "black", order.by = NULL, grid = "white", by.column = TRUE, 
     auto.size = FALSE, mat2 = NULL, plot = TRUE, facet.vars = NULL, 
-    facet.flip = FALSE) {
+    facet.flip = FALSE, diag.na = FALSE) {
     group <- value <- values2 <- NULL
-    
+
+    ## set diagonal to NA
+    if (diag.na) {
+        if (diff(dim(mat)) == 0 && is.matrix(mat)) {
+            diag(mat) <- NA
+        } else {
+            warning ("`mat` is either not a matrix or not symetrical; `diag.na` ignore.")
+        }
+    }
+
     ## convert all numeric matrices
     if (all(sapply(mat, is.numeric))) {
         if (is.matrix(mat)) {
