82c6f2d7e341ca92b71d20162d756bdf0729ebc0
tyler rinker <tyler.rinker@gmail.com>
2013-10-23 10:04:55 -0400

    improved word_cor to handle more than one word and to output a matrix if desired

--- R/word_cor.R
+++ R/word_cor.R
@@ -8,38 +8,83 @@
 #' @param grouping.var The grouping variables.  Default \code{NULL} generates 
 #' one word list for all text.  Also takes a single grouping variable or a list 
 #' of 1 or more grouping variables.
-#' @param word The word to find associated words for.
-#' @param cor The minimum correlation level find associated words for.
-#' @param values logical.  If True returns the named correlates (names are the 
-#' words).  If \code{FALSE} only the associated words are returned.
-#' @return Returns a vector of associated words.
+#' @param word The word(s) vector to find associated words for.
+#' @param r The correlation level find associated words for.  If positive this
+#' is the minimum value, if negative this is the maximum value.
+#' @param values logical.  If \code{TRUE}returns the named correlates (names are 
+#' the words).  If \code{FALSE} only the associated words are returned.
+#' @param \dots Other arguments passed to \code{\link[stats]{cor}}.
+#' @return Returns a vector of associated words or correlation matrix if 
+#' \code{r = NULL}..
 #' @keywords correaltion, association 
 #' @export
 #' @seealso \code{\link[tm]{findAssocs}},
-#' \code{\link[qdap]{word_associate}}
+#' \code{\link[qdap]{word_associate}},
+#' \code{\link[stats]{cor}}
 #' @examples
 #' x <- factor(with(rajSPLIT, paste(act, pad(TOT(tot)), sep = "|")))
 #' word_cor(rajSPLIT$dialogue, x, "romeo", .45)
-#' word_cor(rajSPLIT$dialogue, x, "love", .5)
+#' word_cor(rajSPLIT$dialogue, x, "love", .5)	
+#' 
+#' ## Negative correlation
+#' word_cor(rajSPLIT$dialogue, x, "you", -.1)
 #' with(rajSPLIT, word_cor(dialogue, list(person, act), "hate"))
-word_cor <- function(text.var, grouping.var = ID(text.var), word, cor = .7, values = TRUE) {
+#' 
+#' words <- c("hate", "i", "love", "ghost")
+#' with(rajSPLIT, word_cor(dialogue, x, words, r = .5))
+#' with(rajSPLIT, word_cor(dialogue, x, words, r = .4))
+#' 
+#' ## Set `r = NULL` to get matrix between words
+#' with(rajSPLIT, word_cor(dialogue, x, words, r = NULL))
+word_cor <- function(text.var, grouping.var = ID(text.var), word, r = .7, 
+    values = TRUE, ...) {
 
     WFM <- t(wfm(text.var = text.var, grouping.var = grouping.var))
 
-    WFM <- data.frame(WFM)
-    L <- sapply(WFM[, !colnames(WFM) %in% tolower(word)], function(x) {
-        cor(x, WFM[, tolower(word)])
+    WFM <- data.frame(WFM, check.names = F)
+    wordlen <- length(word) == 1
+
+    if (!is.null(r)) {
+        posit <- r > 0
+        L1 <- lapply(word, cor_help1, m = WFM, o = r, sORw = wordlen, 
+            vals = values, positive = posit, ...)
+        names(L1) <- word
+        L1
+    } else {
+        cor(WFM[, word], ...)
+    }
+}
+
+cor_help1 <- function(n, m, o, sORw, vals, positive, ...) {
+    L <- sapply(m[, !colnames(m) %in% tolower(n)], function(x) {
+        cor(x, m[, tolower(n)], ...)
     })
 
-    extr <- L > cor
-    if (sum(extr) == 0) stop(sprintf("No words correlate at least %s", cor))
-    if (!values) {
+    if (positive) {
+        extr <- L > o
+        phr <- "at least"
+    } else {
+        extr <- L < o
+        phr <- "less than or equal to"
+    }
+
+    if (sum(extr) == 0) {
+        if (sORw) {
+            stop(sprintf("No words correlate %s %s", phr, o))
+        } else {
+            return(NULL)
+        }
+    }
+    if (!vals) {
         names(L)[extr]
     } else {
         L[extr]
     }
-
 }
+
+
+
+
 
 
 
