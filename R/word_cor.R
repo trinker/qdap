06990a65b01b237aa09fca78e5742ced06e359ec
tyler rinker <tyler.rinker@gmail.com>
2014-03-07 17:33:33 -0500

    Added credit to Ben Marwick for plotting method for the list out put of word_cor

--- R/word_cor.R
+++ R/word_cor.R
@@ -18,9 +18,8 @@
 #' \code{r = NULL}.
 #' @keywords correlation, association 
 #' @export
-#' @references The plotting method for the list output was inspired by 
-#' stackoverflow.com member Ben; see 
-#' \url{http://stackoverflow.com/a/19925445/1000343} for more.
+#' @references The plotting method for the list output was inspired by Ben 
+#' Marwick; see \url{http://stackoverflow.com/a/19925445/1000343} for more.
 #' @seealso \code{\link[qdap]{word_proximity}},
 #' \code{\link[tm]{findAssocs}},
 #' \code{\link[qdap]{word_associate}},
