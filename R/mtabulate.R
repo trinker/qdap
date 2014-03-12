e9484de8a489190b066d5493f15baf550d8635b9
tyler rinker <tyler.rinker@gmail.com>
2014-03-12 11:58:58 -0400

    added example mtabulate

--- R/mtabulate.R
+++ R/mtabulate.R
@@ -14,7 +14,13 @@
 #' @examples
 #' mtabulate(list(w=letters[1:10], x=letters[1:5], z=letters))
 #' mtabulate(list(mtcars$cyl[1:10]))
+#' 
+#' ## Dummy coding
 #' mtabulate(mtcars$cyl[1:10])
+#' mtabulate(CO2[, "Plant"])
+#' 
+#' dat <- data.frame(matrix(sample(qcv(A, B), 30, TRUE), ncol=3))
+#' t(mtabulate(dat))
 mtabulate <- function(vects) {
     lev <- sort(unique(unlist(vects)))
     dat <- do.call(rbind, lapply(vects, function(x, lev){ 
