4430816dc66ee8515d69d2d36a2a3a6f67422068
tyler rinker <tyler.rinker@gmail.com>
2014-03-19 22:15:49 -0400

    added discourse_map example

--- R/discourse_map.R
+++ R/discourse_map.R
@@ -44,6 +44,48 @@
 #'     unique(V(mygraph)$family), cols)
 #' 
 #' plot(mygraph, edge.curved=TRUE)
+#' 
+#' ## Repeated Measures (BASIC EXAMPLE)
+#' ##------------------------------
+#' 
+#' ## First merge data and map to discourse per act 
+#' ## to separate networks
+#' 
+#' dat <- key_merge(raj, raj.demographics)
+#' list_dat <- split(dat, dat$act)
+#' plot_dat <- lapply(list_dat, function(x) with(x, discourse_map(dialogue, person)))
+#' 
+#' opar <- par()$mar
+#' par(mfrow=c(3, 2), mar=c(0, 0, 3, 0))
+#' 
+#' lapply(seq_along(plot_dat), function(i){
+#'     plot(plot_dat[[i]])
+#'     mtext(paste("Act", names(plot_dat)[i]), side=3)
+#' })
+#' 
+#' 
+#' ## Repeated Measures (EXTENDED EXAMPLE)
+#' ##------------------------------
+#' 
+#' fam_key <- data.frame(unique(raj.demographics$fam.aff),
+#'     cols = qcv(blue, , grey10, red, brown))
+#' 
+#' par(mfrow=c(3, 2), mar=c(0, 1, 3, 1))
+#' lapply(seq_along(plot_dat), function(i){
+#' 
+#'     THE_PLOT <- visual(plot_dat[[i]])
+#' 
+#'     V(THE_PLOT)$sex <- V(THE_PLOT)$name %l% raj.demographics[, 1:2]
+#'     V(THE_PLOT)$color <- ifelse(V(THE_PLOT)$sex=="f", "pink", "lightblue")
+#'     V(THE_PLOT)$family <- V(THE_PLOT)$name %l+% raj.demographics[, c(1, 3)]
+#'     V(THE_PLOT)$label.color <- lookup(V(THE_PLOT)$family, fam_key)
+#' 
+#'     plot(THE_PLOT, edge.curved=TRUE)
+#'     mtext(paste("Act", names(plot_dat)[i]), side=3)
+#' })
+#' 
+#' ## Reset graphics margins
+#' par(mar=opar)
 #' }
 discourse_map <- function(text.var, grouping.var, edge.constant, sep = "_", 
     ...) {
