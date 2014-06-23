cb53c0293e4bbe3f671a5310c6e97fc5df2ca480
trinker <tyler.rinker@gmail.com>
2014-06-23 17:39:18 -0400

    made message a warning so stronger

--- R/as.tdm.R
+++ R/as.tdm.R
@@ -528,7 +528,7 @@ as.data.frame.Corpus <- function(x, row.names, optional, ..., doc = "docs",
 
     if (sent.split) {
         if(any(end_mark(out[["text"]]) == "_")) {
-            message("Missing end marks.", "Consider setting:\n\n    sent.split = FALSE\n")
+            warning("Missing end marks.  Consider setting: sent.split = FALSE\n")
         }        
         out <- sentSplit(out, text, ...)
     }
