ec29cb1548ced6ceac40600c02dd96028c8b27bc
trinker <tyler.rinker@gmail.com>
2015-04-20 12:52:35 -0400

    Added unit test add_incomplete and updated DESCRIPTION to conform to CRAN expectations

--- tests/testthat/test-add_incomplete.R
+++ tests/testthat/test-add_incomplete.R
@@ -1,7 +1,20 @@
 context("Checking add_incomplete")
 
-test_that("add_incomplete ...",{
+test_that("add_incomplete adds `|` to no punctuation sentences.",{
 
-
+    expected <- c("This in a|", "I am funny!", "An ending of sorts%|", "What do you want?")
+    
+    x <- add_incomplete(
+        c(
+            "This in a",
+            "I am funny!",
+            "An ending of sorts%",
+            "What do you want?"
+        ), 
+        silent = TRUE
+    )
+    
+    expect_equal(x, expected)
+    
 })
 
