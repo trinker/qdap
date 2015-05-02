e8cd59aae73aaf626c5091ef2c2b684cfcecb715
Tyler <tyler.rinker@gmail.com>
2015-05-02 12:47:13 -0400

    fixed many-eyes link that won't pass cran checks

--- R/phrase_net.R
+++ R/phrase_net.R
@@ -1,6 +1,6 @@
 #' Phrase Nets
 #' 
-#' Create \href{http://www-958.ibm.com/software/data/cognos/manyeyes/}{Many Eyes} 
+#' Create \href{http://trinker.github.io/many-eye}{Many Eyes} 
 #' style phrase nets.
 #' 
 #' @param text.var The text variable.
@@ -10,11 +10,11 @@
 #' @param vertex.constant A constant to multiple the vertex label sizes by.
 #' @param \ldots Other arguments passed to \code{\link[qdap]{Filter}}.
 #' @return Returns an igraph object.
-#' @note While \href{http://www-958.ibm.com/software/data/cognos/manyeyes/}{Many Eyes} 
+#' @note While \href{http://trinker.github.io/many-eye}{Many Eyes} 
 #' phrase nets inspired this function the two outputs are not identical.  The
 #' \code{\link[qdap]{phrase_net}} function operates off of correlations between 
 #' words in sentences.
-#' @references \url{http://www-01.ibm.com}
+#' @references \url{http://trinker.github.io/many-eye}
 #' @keywords phrase_net
 #' @export
 #' @importFrom reshape2 melt
