c70917ffedac90cb52fcc7b087527cbf1b5ef13c
Tyler <tyler.rinker@gmail.com>
2017-04-14 09:17:22 -0400

    upped

--- inst/build.R
+++ inst/build.R
@@ -1,39 +1,20 @@
-root <- switch(Sys.info()[["user"]],
-    Tyler = "C:/Users/Tyler",
-    trinker = "C:/Users/trinker", 
-    message("Computer name not found")
-)
-
-repo <- pack <- basename(getwd())
-
-curd <- getwd()
-loc <- file.path(root, "Desktop")
-setwd(loc)
-
-base.git <- file.path(root, "GitHub")
-
-
-qman <- function(x = repo, db = file.path(root, "/Dropbox/Public"), dir=loc) {
-    path <- file.path(dir, paste0(x, ".pdf"))
-    if (!file.exists(path)) stop(paste(x, "does not exist..."))
-    opath <- file.path(db, paste0(x, ".pdf"))
-    file.copy(path, opath, overwrite = TRUE)
-    message("manual copied!\n")
-}
-
+root <- Sys.getenv("USERPROFILE")
+pack <- basename(getwd())
 
 quick <-  TRUE
-library(devtools)
+pdf <- TRUE
 
 unlink(paste0(pack, ".pdf"), recursive = TRUE, force = TRUE)
-x <- file.path(base.git, pack)
-document(x)
-install(x, quick = quick, build_vignettes = FALSE, dependencies = TRUE)
+devtools::document()
+devtools::install(quick = quick, build_vignettes = FALSE, dependencies = TRUE)
 
-path <- find.package(pack)
-system(paste(shQuote(file.path(R.home("bin"), "R")), 
-    "CMD", "Rd2pdf", shQuote(path)))
+if(pdf){
+    path <- find.package(pack)
+    system(paste(shQuote(file.path(R.home("bin"), "R")), "CMD", "Rd2pdf", shQuote(path)))
+    file.copy(paste0(pack, '.pdf'), file.path(root,"Desktop", paste0(pack, '.pdf')))
+    while (file.exists(paste0(pack, ".pdf"))) {unlink(paste0(pack, ".pdf"), recursive = TRUE, force = TRUE)}
+    empts <- grep("^\\.Rd", dir(all.files = TRUE), value = TRUE)
+    unlink(empts, recursive = TRUE, force = TRUE)
+}
 
-qman(repo, dir=loc)
-setwd(curd)
 message("Done!")
